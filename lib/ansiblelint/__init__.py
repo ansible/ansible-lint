57b4b51ac1f341a8584433a879949a76d8f375ad
Will Thames <will@thames.id.au>
2013-08-19 22:39:52 +1000

    Added new Match class
    Needs more tests! wip, untested, may not compile

--- lib/ansiblelint/__init__.py
+++ lib/ansiblelint/__init__.py
@@ -31,8 +31,40 @@ class RulesCollection(object):
     def __len__(self):
         return len(self.rules)
 
+    def run(self, playbookfile, tags=None, skip_tags=None):
+        text = ""
+        matches = list()
+        with open(playbookfile, 'r') as f:
+            text = f.read()
+        for rule in self.rules:
+            if not tags or rule.tag in tags:
+                if not skip_tags or rule.tag not in skip_tags:
+                    matches.extend(Match.from_matches(filename, rule, text))
+                    matches.extend(rule.postmatch(text))
+        return matches
+
     @classmethod
     def create_from_directory(cls, rulesdir):
         result = cls()
         result.rules = utils.load_plugins(rulesdir)
+        return result
+
+
+class Match:
+
+    def __init__(self, linenumber, line, filename, rule):
+        self.linenumber = linenumber
+        self.line = line
+        self.filename = filename
+        self.rule = rule
+
+    def __repr__(self):
+        return "[{}] ({}) matched {}:{} {}" % (rule.id, rule.description,
+                                               filename, linenumber, line)
+
+    @staticmethod
+    def from_matches(filename, rule, text):
+        result = list()
+        for match in rule.prematch(text).extend(rule.postmatch(text)):
+            result.append(match, text.split("\n")[match-1], filename, rule)
         return result
