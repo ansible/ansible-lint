2fbc9d6992f6f0e5dd651228f266d8096158a8d0
Michael Villis <michael.villis@suncorp.com.au>
2014-08-08 14:10:08 +1000

    Added indices to format strings for python 2.6 support

--- lib/ansiblelint/formatters/__init__.py
+++ lib/ansiblelint/formatters/__init__.py
@@ -1,7 +1,7 @@
 class Formatter:
     
     def format(self, match):
-        formatstr = "[{}] {}\n{}:{}\n{}\n"
+        formatstr = "[{0}] {1}\n{2}:{3}\n{4}\n"
         return formatstr.format(match.rule.id,
                                 match.message,
                                 match.filename, 
@@ -11,6 +11,6 @@ class Formatter:
 class QuietFormatter:
 
     def format(self, match):
-        formatstr = "[{}] {}:{}"
+        formatstr = "[{0}] {1}:{2}"
         return formatstr.format(match.rule.id, match.filename, 
                                 match.linenumber)
