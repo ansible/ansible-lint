0f22ee78b1ede999430d3c72dd58ea31cacad99a
Sorin Sbarnea <sorinsbarnea@bmlltech.com>
2016-04-07 21:11:13 +0100

    Implemented version load based on pip package approach.

--- setup.py
+++ setup.py
@@ -2,12 +2,31 @@
 from __future__ import absolute_import
 from setuptools import find_packages
 from setuptools import setup
+import codecs
+import os
+import re
 
-from lib.ansiblelint import __version__
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 setup(
     name='ansible-lint',
-    version=__version__,
+    version=find_version("lib/ansiblelint", "version.py"),
     description=('checks playbooks for practices and behaviour that could potentially be improved'),
     keywords='ansible, lint',
     author='Will Thames',
