e0d0059381284d939861150bed7b01a8366dea90
Florian Dambrine <android.florian@gmail.com>
2016-06-28 04:29:42 -0600

    Adding colored output formatter (#167)
    * Adding colored output formatter, thanks to @Lowess 
    
    * Using sringc routine from ansible-core
    
    * Removing extra line from utils.py
    
    * Fixing flake8 complaints + moving stringc imports in utils.py
    
    * Make colored outputs available for all exisiting formatters
    
    * Changing -c to --nocolor + make colored=False in format method in order to simplify tests calls
    
    * Removing extra space

--- lib/ansiblelint/formatters/__init__.py
+++ lib/ansiblelint/formatters/__init__.py
@@ -1,28 +1,51 @@
+try:
+    from ansible.color import stringc
+except ImportError:
+    from ansible.utils.color import stringc
+
+
 class Formatter(object):
 
-    def format(self, match):
-        formatstr = u"[{0}] {1}\n{2}:{3}\n{4}\n"
-        return formatstr.format(match.rule.id,
-                                match.message,
-                                match.filename,
-                                match.linenumber,
-                                match.line)
+    def format(self, match, colored=False):
+        formatstr = u"{0} {1}\n{2}:{3}\n{4}\n"
+        if colored:
+            return formatstr.format(stringc(u"[{0}]".format(match.rule.id), 'bright red'),
+                                    stringc(match.message, 'red'),
+                                    stringc(match.filename, 'blue'),
+                                    stringc(match.linenumber, 'cyan'),
+                                    stringc(match.line, 'magenta'))
+        else:
+            return formatstr.format(match.rule.id,
+                                    match.message,
+                                    match.filename,
+                                    match.linenumber,
+                                    match.line)
 
 
 class QuietFormatter(object):
 
-    def format(self, match):
-        formatstr = u"[{0}] {1}:{2}"
-        return formatstr.format(match.rule.id, match.filename,
-                                match.linenumber)
+    def format(self, match, colored=False):
+        formatstr = u"{0} {1}:{2}"
+        if colored:
+            return formatstr.format(stringc(u"[{0}]".format(match.rule.id), 'bright red'),
+                                    stringc(match.filename, 'blue'),
+                                    stringc(match.linenumber, 'cyan'))
+        else:
+            return formatstr.format(match.rule.id, match.filename,
+                                    match.linenumber)
 
 
 class ParseableFormatter(object):
 
-    def format(self, match):
+    def format(self, match, colored=False):
         formatstr = u"{0}:{1}: [{2}] {3}"
-        return formatstr.format(match.filename,
-                                match.linenumber,
-                                "E" + match.rule.id,
-                                match.message,
-                                )
+        if colored:
+            return formatstr.format(stringc(match.filename, 'blue'),
+                                    stringc(match.linenumber, 'cyan'),
+                                    stringc(u"E{0}".format(match.rule.id), 'bright red'),
+                                    stringc(match.message, 'red'))
+        else:
+            return formatstr.format(match.filename,
+                                    match.linenumber,
+                                    "E" + match.rule.id,
+                                    match.message)
