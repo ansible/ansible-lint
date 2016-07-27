00fbe717eeb93dc4eddab4fbd3eedfdaa31859ec
Will Thames <will@thames.id.au>
2016-07-27 20:10:02 +1000

    Test stringc to avoid future failures (#178)
    * Add test for stringc formatting
    
    * Allow testing of stringc
    
    Make ANSIBLE_COLOR True when coloured formatting is
    required.
    
    * Fix stringc calls to not pass ints
    
    Ansible 1.9's `stringc` fails when passed an `int`
    or `dict. Wrap `linenumber` and `line`  in `str`
    before calling `stringc`.
    
    We hit the `dict` problem with SudoRule as it outputs
    a `dict` line - this might be a bug, but easier to
    fix the formatter than change rules
    
    * Update version to 3.2.2

--- lib/ansiblelint/formatters/__init__.py
+++ lib/ansiblelint/formatters/__init__.py
@@ -1,7 +1,7 @@
 try:
-    from ansible.color import stringc
+    from ansible import color
 except ImportError:
-    from ansible.utils.color import stringc
+    from ansible.utils import color
 
 
 class Formatter(object):
@@ -9,11 +9,12 @@ class Formatter(object):
     def format(self, match, colored=False):
         formatstr = u"{0} {1}\n{2}:{3}\n{4}\n"
         if colored:
-            return formatstr.format(stringc(u"[{0}]".format(match.rule.id), 'bright red'),
-                                    stringc(match.message, 'red'),
-                                    stringc(match.filename, 'blue'),
-                                    stringc(match.linenumber, 'cyan'),
-                                    stringc(match.line, 'purple'))
+            color.ANSIBLE_COLOR = True
+            return formatstr.format(color.stringc(u"[{0}]".format(match.rule.id), 'bright red'),
+                                    color.stringc(match.message, 'red'),
+                                    color.stringc(match.filename, 'blue'),
+                                    color.stringc(str(match.linenumber), 'cyan'),
+                                    color.stringc(str(match.line), 'purple'))
         else:
             return formatstr.format(match.rule.id,
                                     match.message,
@@ -27,9 +28,10 @@ class QuietFormatter(object):
     def format(self, match, colored=False):
         formatstr = u"{0} {1}:{2}"
         if colored:
-            return formatstr.format(stringc(u"[{0}]".format(match.rule.id), 'bright red'),
-                                    stringc(match.filename, 'blue'),
-                                    stringc(match.linenumber, 'cyan'))
+            color.ANSIBLE_COLOR = True
+            return formatstr.format(color.stringc(u"[{0}]".format(match.rule.id), 'bright red'),
+                                    color.stringc(match.filename, 'blue'),
+                                    color.stringc(str(match.linenumber), 'cyan'))
         else:
             return formatstr.format(match.rule.id, match.filename,
                                     match.linenumber)
@@ -40,10 +42,11 @@ class ParseableFormatter(object):
     def format(self, match, colored=False):
         formatstr = u"{0}:{1}: [{2}] {3}"
         if colored:
-            return formatstr.format(stringc(match.filename, 'blue'),
-                                    stringc(match.linenumber, 'cyan'),
-                                    stringc(u"E{0}".format(match.rule.id), 'bright red'),
-                                    stringc(match.message, 'red'))
+            color.ANSIBLE_COLOR = True
+            return formatstr.format(color.stringc(match.filename, 'blue'),
+                                    color.stringc(str(match.linenumber), 'cyan'),
+                                    color.stringc(u"E{0}".format(match.rule.id), 'bright red'),
+                                    color.stringc(match.message, 'red'))
         else:
             return formatstr.format(match.filename,
                                     match.linenumber,
