47dfdc9226ddf6f358f7a0a5ca31e10cd828f5c2
Vesa Kaihlavirta <vesa.kaihlavirta@qvantel.com>
2017-04-25 13:33:14 +0300

    Reorder test data. Register the rule to be tested

--- test/TestRoleHandlers.py
+++ test/TestRoleHandlers.py
@@ -1,11 +1,16 @@
 import unittest
 from ansiblelint import Runner, RulesCollection
+from ansiblelint.rules.UseHandlerRatherThanWhenChangedRule import UseHandlerRatherThanWhenChangedRule
 
 class TestRoleHandlers(unittest.TestCase):
     collection = RulesCollection()
+
+    def setUp(self):
+        self.collection.register(UseHandlerRatherThanWhenChangedRule())
 
     def test_role_handler_positive(self):
         success = 'test/252/main.yml'
         good_runner = Runner(self.collection, success, [], [], [])
         self.assertEqual([], good_runner.run())
+        print(dir(good_runner))
 
