f46f48b8b9fc7a528d752c9348ab657b8484cb9c
Will Thames <will@thames.id.au>
2018-03-23 08:31:22 +1000

    Improve readability and test coverage of octal permissions
    Enumerate all the valid modes that we expect to be able to
    detect and the invalid modes we expect to detect.
    
    Make the OctalPermissionsRule a lot more readable and remove
    a lot of bugs spotted while improving tests and making it readable!

--- lib/ansiblelint/rules/OctalPermissionsRule.py
+++ lib/ansiblelint/rules/OctalPermissionsRule.py
@@ -37,25 +37,36 @@ class OctalPermissionsRule(AnsibleLintRule):
     mode_regex = re.compile(r'^\s*[0-9]+\s*$')
     valid_mode_regex = re.compile(r'^\s*0[0-7]{3,4}\s*$')
 
+    def is_invalid_permission(self, mode):
+        # sensible file permission modes don't
+        # have write bit set when read bit is
+        # not set and don't have execute bit set
+        # when user execute bit is not set.
+        # also, user permissions are more generous than
+        # group permissions and user and group permissions
+        # are more generous than world permissions
+
+        other_write_without_read = (mode % 8 and mode % 8 < 4 and
+                                    not (mode % 8 == 1 and (mode >> 6) % 2 == 1))
+        group_write_without_read = ((mode >> 3) % 8 and (mode >> 3) % 8 < 4 and
+                                    not ((mode >> 3) % 8 == 1 and (mode >> 6) % 2 == 1))
+        user_write_without_read = ((mode >> 6) % 8 and (mode >> 6) % 8 < 4 and
+                                   not (mode >> 6) % 8 == 1)
+        other_more_generous_than_group = mode % 8 > (mode >> 3) % 8
+        other_more_generous_than_user = mode % 8 > (mode >> 6) % 8
+        group_more_generous_than_user = (mode >> 3) % 8 > (mode >> 6) % 8
+
+        return (other_write_without_read or
+                group_write_without_read or
+                user_write_without_read or
+                other_more_generous_than_group or
+                other_more_generous_than_user or
+                group_more_generous_than_user)
+
     def matchtask(self, file, task):
         if task["action"]["__ansible_module__"] in self._modules:
             mode = task['action'].get('mode', None)
             if isinstance(mode, six.string_types) and self.mode_regex.match(mode):
                 return not self.valid_mode_regex.match(mode)
             if isinstance(mode, int):
-                # sensible file permission modes don't
-                # have write bit set when read bit is
-                # not set and don't have execute bit set
-                # when user execute bit is not set.
-                # also, user permissions are more generous than
-                # group permissions and user and group permissions
-                # are more generous than world permissions
-
-                result = (mode % 8 and mode % 8 < 4 and not (mode % 8 == 1 and (mode >> 6) % 2 == 1) or
-                          (mode >> 3) % 8 and (mode >> 3) % 8 < 4 and not ((mode >> 3) % 8 == 1 and (mode >> 6) % 2 == 1) or
-                          (mode >> 6) % 8 and (mode >> 6) % 8 < 4 and not (mode >> 6) % 8 == 1 or
-                          mode & 8 < (mode << 3) & 8 or
-                          mode & 8 < (mode << 6) & 8 or
-                          (mode << 3) & 8 < (mode << 6) & 8)
-
-                return result
+                return self.is_invalid_permission(mode)
--- test/TestOctalPermissions.py
+++ test/TestOctalPermissions.py
@@ -5,9 +5,19 @@ from ansiblelint.rules.OctalPermissionsRule import OctalPermissionsRule
 
 class TestOctalPermissionsRuleWithFile(unittest.TestCase):
     collection = RulesCollection()
+    VALID_MODES = [ 0777, 0775, 0770, 0755, 0750, 0711, 0710, 0700,
+                    0666, 0664, 0660, 0644, 0640, 0600,
+                    0555, 0551, 0550, 0511, 0510, 0500,
+                    0444, 0440, 0400 ]
+
+    INVALID_MODES = [ 777, 775, 770, 755, 750, 711, 710, 700,
+                    666, 664, 660, 644, 640, 622, 620, 600,
+                    555, 551, 550, # 511 == 0777, 510 == 0776, 500 == 0764
+                    444, 440, 400 ]
 
     def setUp(self):
-        self.collection.register(OctalPermissionsRule())
+        self.rule = OctalPermissionsRule()
+        self.collection.register(self.rule)
 
     def test_file_positive(self):
         success = 'test/octalpermissions-success.yml'
@@ -19,3 +29,11 @@ class TestOctalPermissionsRuleWithFile(unittest.TestCase):
         bad_runner = Runner(self.collection, failure, [], [], [])
         errs = bad_runner.run()
         self.assertEqual(5, len(errs))
+
+    def test_valid_modes(self):
+        for mode in self.VALID_MODES:
+            self.assertFalse(self.rule.is_invalid_permission(mode), msg="0%o should be a valid mode" % mode)
+
+    def test_invalid_modes(self):
+        for mode in self.INVALID_MODES:
+            self.assertTrue(self.rule.is_invalid_permission(mode), msg="%d should be an invalid mode" % mode)
