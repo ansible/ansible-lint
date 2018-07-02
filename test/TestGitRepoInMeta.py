71fad831853c9a0723327fe0698892c538125a03
Johannes Brunswicker <johannes.brunswicker@gmail.com>
2018-07-02 15:36:08 +0200

    * added testcase

---
+++ test/TestGitRepoInMeta.py
@@ -0,0 +1,18 @@
+import unittest
+import ansible
+
+from ansiblelint import Runner, RulesCollection
+from ansiblelint.rules.EnvVarsInCommandRule import EnvVarsInCommandRule
+from pkg_resources import parse_version
+
+
+class TestEnvVarsInCommand(unittest.TestCase):
+    collection = RulesCollection()
+
+    def setUp(self):
+        self.collection.register(EnvVarsInCommandRule())
+
+    def test_file_positive(self):
+        success = 'test/git-dependency-in-meta/main.yml.yml'
+        good_runner = Runner(self.collection, success, [], [], [])
+        self.assertEqual([], good_runner.run())
