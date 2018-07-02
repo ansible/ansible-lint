67e0c56479ebc93273968eacb9007f54fb108631
Johannes Brunswicker <johannes.brunswicker@gmail.com>
2018-07-02 16:47:46 +0200

    * blind test

--- test/TestDependenciesInMeta.py
+++ test/TestDependenciesInMeta.py
@@ -12,25 +12,25 @@ class TestDependenciesInMeta(unittest.TestCase):
         self.rules = RulesCollection.create_from_directory(rulesdir)
 
     def bitbucket_in_meta_dependency_is_ok(self):
-        success = 'test/dependency-in-meta/bitbucket.yml'
-        self.assertEqual([], Runner(self.collection, success, [], [], []).run())
+        filename = 'test/dependency-in-meta/bitbucket.yml'
+        self.assertEqual([], Runner(self.rules, filename, [], [], []).run())
 
     def galaxy_dependency_is_ok(self):
-        success = 'test/dependency-in-meta/galaxy.yml'
-        good_runner = Runner(self.collection, success, [], [], [])
+        filename = 'test/dependency-in-meta/galaxy.yml'
+        good_runner = Runner(self.rules, filename, [], [], [])
         self.assertEqual([], good_runner.run())
 
     def github_dependency_is_ok(self):
-        success = 'test/dependency-in-meta/github.yml'
-        good_runner = Runner(self.collection, success, [], [], [])
+        filename = 'test/dependency-in-meta/github.yml'
+        good_runner = Runner(self.rules, filename, [], [], [])
         self.assertEqual([], good_runner.run())
 
     def webserver_dependency_is_ok(self):
-        success = 'test/dependency-in-meta/webserver.yml'
-        good_runner = Runner(self.collection, success, [], [], [])
+        filename = 'test/dependency-in-meta/webserver.yml'
+        good_runner = Runner(self.rules, filename, [], [], [])
         self.assertEqual([], good_runner.run())
 
     def gitlab_dependency_is_ok(self):
-        success = 'test/dependency-in-meta/gitlab.yml'
-        good_runner = Runner(self.collection, success, [], [], [])
+        filename = 'test/dependency-in-meta/gitlab.yml'
+        good_runner = Runner(self.rules, filename, [], [], [])
         self.assertEqual([], good_runner.run())
