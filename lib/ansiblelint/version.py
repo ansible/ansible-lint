5d2f573485d404cf0472a554832c98332850b2ac
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-10-30 01:44:32 +0100

    Switch to using setuptools-scm
    Also:
    * Unshallow Git repo in CI to make setuptools-scm work well
    
    Signed-off-by: Sviatoslav Sydorenko <wk@sydorenko.org.ua>

--- lib/ansiblelint/version.py
+++ lib/ansiblelint/version.py
@@ -1,1 +1,15 @@
-__version__ = '3.5.1'
+"""Ansible-lint version information."""
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+try:
+    import pkg_resources
+except ImportError:
+    pass
+
+
+try:
+    __version__ = pkg_resources.get_distribution('ansible-lint').version
+except Exception:
+    __version__ = 'unknown'
