7b82713d44b18101e1295579c3fb8035c30ddaae
Andrew Crosby <acrosby@redhat.com>
2018-10-31 14:26:37 -0400

    Changed LineTooLongRule to 120 chars
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

--- lib/ansiblelint/rules/LineTooLongRule.py
+++ lib/ansiblelint/rules/LineTooLongRule.py
@@ -6,10 +6,10 @@ from ansiblelint import AnsibleLintRule
 
 class LineTooLongRule(AnsibleLintRule):
     id = '204'
-    shortdesc = 'Lines should be no longer than 160 chars'
+    shortdesc = 'Lines should be no longer than 120 chars'
     description = 'Long lines make code harder to read and ' \
                   'code review more difficult'
     tags = ['formatting']
 
     def match(self, file, line):
-        return len(line) > 160
+        return len(line) > 120
