2f0e48debfe6c5d8e926fc04398e2cf28067c08e
Andrew Crosby <acrosby@redhat.com>
2018-11-13 22:32:29 -0500

    Added class to run tests from text, keeping test cases in one file
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

--- test/TestSudoRule.py
+++ test/TestSudoRule.py
@@ -1,21 +1,66 @@
 import unittest
 
-from ansiblelint import Runner, RulesCollection
+from ansiblelint import RulesCollection
 from ansiblelint.rules.SudoRule import SudoRule
+from test import RunFromText
+
+ROLE_2_ERRORS = '''
+- name: test
+  debug:
+    msg: 'test message'
+  sudo: yes
+  sudo_user: nobody
+'''
+
+ROLE_0_ERRORS = '''
+- name: test
+  debug:
+    msg: 'test message'
+  become: yes
+  become_user: somebody
+'''
+
+PLAY_4_ERRORS = '''
+- hosts: all
+  sudo: yes
+  sudo_user: somebody
+  tasks:
+  - name: test
+    debug:
+      msg: 'test message'
+    sudo: yes
+    sudo_user: nobody
+'''
+
+PLAY_1_ERROR = '''
+- hosts: all
+  tasks:
+  - name: test
+    debug:
+      msg: 'test message'
+    sudo: yes
+'''
 
 
 class TestSudoRule(unittest.TestCase):
     collection = RulesCollection()
     collection.register(SudoRule())
 
-    def test_file(self):
-        file = 'test/sudo.yml'
-        runner = Runner(self.collection, file, [], [], [])
-        results = runner.run()
+    def setUp(self):
+        self.runner = RunFromText(self.collection)
+
+    def test_run_role_fail(self):
+        results = self.runner.run_role_tasks_main(ROLE_2_ERRORS)
+        self.assertEqual(2, len(results))
+
+    def test_run_role_pass(self):
+        results = self.runner.run_role_tasks_main(ROLE_0_ERRORS)
+        self.assertEqual(0, len(results))
+
+    def test_play_root_and_task_fail(self):
+        results = self.runner.run_playbook(PLAY_4_ERRORS)
         self.assertEqual(4, len(results))
 
-    def test_role(self):
-        role_path = 'test/role-sudo'
-        runner = Runner(self.collection, role_path, [], [], [])
-        results = runner.run()
-        self.assertEqual(2, len(results))
+    def test_play_task_fail(self):
+        results = self.runner.run_playbook(PLAY_1_ERROR)
+        self.assertEqual(1, len(results))
---
+++ test/__init__.py
@@ -0,0 +1,34 @@
+import tempfile
+import shutil
+import os
+
+from ansiblelint import Runner
+
+
+class RunFromText(object):
+    """Use Runner on temp files created from unittest text snippets."""
+
+    def __init__(self, collection):
+        self.collection = collection
+
+    def _call_runner(self, path):
+        runner = Runner(self.collection, path, [], [], [])
+        return runner.run()
+
+    def run_playbook(self, playbook_text):
+        with tempfile.NamedTemporaryFile() as fp:
+            fp.write(playbook_text.encode())
+            fp.seek(0)
+            results = self._call_runner(fp.name)
+        return results
+
+    def run_role_tasks_main(self, tasks_main_text):
+        role_path = tempfile.mkdtemp(prefix='role_')
+        tasks_path = os.path.join(role_path, 'tasks')
+        os.makedirs(tasks_path)
+        with open(os.path.join(tasks_path, 'main.yml'), 'w') as fp:
+            fp.write(tasks_main_text)
+            fp.seek(0)
+            results = self._call_runner(role_path)
+        shutil.rmtree(role_path)
+        return results
