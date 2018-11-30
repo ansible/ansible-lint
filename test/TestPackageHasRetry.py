c95989f5bf11d527638d97375996757e32f0596a
Andrew Crosby <acrosby@redhat.com>
2018-11-29 20:30:16 -0500

    Add Package has retry rule
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

---
+++ test/TestPackageHasRetry.py
@@ -0,0 +1,44 @@
+import unittest
+
+from ansiblelint import RulesCollection
+from ansiblelint.rules.PackageHasRetryRule import PackageHasRetryRule
+from test import RunFromText
+
+SUCCESS = '''
+---
+- hosts: hosts
+  tasks:
+    - name: "Package has retry test success"
+      apt:
+        pkg: foo
+      register: result
+      until: result|success
+'''
+
+FAILURE = '''
+---
+- hosts: hosts
+  tasks:
+    - name: "Package has retry test failure"
+      apt:
+        pkg: foo
+    - name: "Package has retry test failure"
+      macports:
+        pkg: foo
+'''
+
+
+class TestPackageHasRetry(unittest.TestCase):
+    collection = RulesCollection()
+    collection.register(PackageHasRetryRule())
+
+    def setUp(self):
+        self.runner = RunFromText(self.collection)
+
+    def test_file_positive(self):
+        results = self.runner.run_playbook(SUCCESS)
+        self.assertEqual(0, len(results))
+
+    def test_file_negative(self):
+        results = self.runner.run_playbook(FAILURE)
+        self.assertEqual(2, len(results))
