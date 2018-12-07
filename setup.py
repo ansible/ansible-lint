7ad8dc5873c51382a23116974ced8824c463aa44
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-12-07 16:22:54 +0100

    Cut off local version identifier on upload

--- setup.py
+++ setup.py
@@ -132,4 +132,19 @@ setup_params = dict(setup_params, **declarative_setup_params['metadata'])
 setup_params = dict(setup_params, **declarative_setup_params['options'])
 
 
+def cut_local_version_on_upload(version):
+    import os
+    import setuptools_scm.version  # only present during setup time
+    IS_PYPI_UPLOAD = os.getenv('PYPI_UPLOAD') == 'true'
+    return (
+        '' if IS_PYPI_UPLOAD
+        else setuptools_scm.version.get_local_node_and_date(version)
+    )
+
+
+setup_params['use_scm_version'] = {
+    'local_scheme': cut_local_version_on_upload,
+}
+
+
 __name__ == '__main__' and setuptools.setup(**setup_params)
