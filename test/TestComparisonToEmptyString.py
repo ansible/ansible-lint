880ad4918e8c5a6572cd6dc6639f3f4cb0f810af
Andrew Crosby <acrosby@redhat.com>
2019-01-02 16:39:19 -0500

    Improve regex on rule 602
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

--- test/TestComparisonToEmptyString.py
+++ test/TestComparisonToEmptyString.py
@@ -5,24 +5,21 @@ from ansiblelint.rules.ComparisonToEmptyStringRule import (
     ComparisonToEmptyStringRule)
 from test import RunFromText
 
-PASS_WHEN = '''
+SUCCESS_TASKS = '''
 - name: shut down
   command: /sbin/shutdown -t now
   when: ansible_os_family
 '''
 
-FAIL_IS_EMPTY = '''
+FAIL_TASKS = '''
 - hosts: all
   tasks:
   - name: shut down
     command: /sbin/shutdown -t now
     when: ansible_os_family == ""
-'''
-
-FAIL_IS_NOT_EMPTY = '''
-- name: shut down
-  command: /sbin/shutdown -t now
-  when: ansible_os_family != ""
+  - name: shut down
+    command: /sbin/shutdown -t now
+    when: ansible_os_family !=""
 '''
 
 
@@ -33,14 +30,10 @@ class TestComparisonToEmptyStringRule(unittest.TestCase):
     def setUp(self):
         self.runner = RunFromText(self.collection)
 
-    def test_compare_when(self):
-        results = self.runner.run_role_tasks_main(PASS_WHEN)
+    def test_success(self):
+        results = self.runner.run_role_tasks_main(SUCCESS_TASKS)
         self.assertEqual(0, len(results))
 
-    def test_compare_is_empty(self):
-        results = self.runner.run_playbook(FAIL_IS_EMPTY)
-        self.assertEqual(1, len(results))
-
-    def test_compare_is_not_empty(self):
-        results = self.runner.run_role_tasks_main(FAIL_IS_NOT_EMPTY)
-        self.assertEqual(1, len(results))
+    def test_fail(self):
+        results = self.runner.run_playbook(FAIL_TASKS)
+        self.assertEqual(2, len(results))
