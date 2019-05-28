2a92ef190df378ac71fd3f02dacb06e3a5ea26ab
Andrew Crosby <acrosby@redhat.com>
2019-05-28 12:23:22 -0400

    Fix to look for noqa skips in handlers pre and post tasks (#520)
    Signed-off-by: Andrew Crosby <acrosby@redhat.com>

---
+++ test/TestSkipPlaybookItems.py
@@ -0,0 +1,88 @@
+import unittest
+import os
+
+from ansiblelint import RulesCollection
+from test import RunFromText
+
+
+PLAYBOOK_PRE_TASKS = '''
+- hosts: all
+  tasks:
+    - name: bad git 1  # noqa 401
+      action: git a=b c=d
+    - name: bad git 2
+      action: git a=b c=d
+  pre_tasks:
+    - name: bad git 3  # noqa 401
+      action: git a=b c=d
+    - name: bad git 4
+      action: git a=b c=d
+'''
+
+PLAYBOOK_POST_TASKS = '''
+- hosts: all
+  tasks:
+    - name: bad git 1  # noqa 401
+      action: git a=b c=d
+    - name: bad git 2
+      action: git a=b c=d
+  post_tasks:
+    - name: bad git 3  # noqa 401
+      action: git a=b c=d
+    - name: bad git 4
+      action: git a=b c=d
+'''
+
+PLAYBOOK_HANDLERS = '''
+- hosts: all
+  tasks:
+    - name: bad git 1  # noqa 401
+      action: git a=b c=d
+    - name: bad git 2
+      action: git a=b c=d
+  handlers:
+    - name: bad git 3  # noqa 401
+      action: git a=b c=d
+    - name: bad git 4
+      action: git a=b c=d
+'''
+
+PLAYBOOK_TWO_PLAYS = '''
+- hosts: all
+  tasks:
+    - name: bad git 1  # noqa 401
+      action: git a=b c=d
+    - name: bad git 2
+      action: git a=b c=d
+
+- hosts: all
+  tasks:
+    - name: bad git 3  # noqa 401
+      action: git a=b c=d
+    - name: bad git 4
+      action: git a=b c=d
+'''
+
+
+class TestSkipPlaybookItems(unittest.TestCase):
+    rulesdir = os.path.join('lib', 'ansiblelint', 'rules')
+    collection = RulesCollection.create_from_directory(rulesdir)
+
+    def setUp(self):
+        self.runner = RunFromText(self.collection)
+
+    def test_pre_tasks(self):
+        results = self.runner.run_playbook(PLAYBOOK_PRE_TASKS)
+        self.assertEqual(2, len(results))
+
+    def test_post_tasks(self):
+        results = self.runner.run_playbook(PLAYBOOK_POST_TASKS)
+        self.assertEqual(2, len(results))
+
+    def test_play_handlers(self):
+        results = self.runner.run_playbook(PLAYBOOK_HANDLERS)
+        self.assertEqual(2, len(results))
+
+    def test_two_plays(self):
+        results = self.runner.run_playbook(PLAYBOOK_TWO_PLAYS)
+        self.assertEqual(2, len(results))
