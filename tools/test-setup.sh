5145d79afcb238389ddd3aa1a5206286502491e2
Sorin Sbarnea <ssbarnea@redhat.com>
2020-01-17 22:09:15 +0000

    Enable Zuul CI Jobs
    These Zuul CI jobs will run in parallel till we get enough history
    to confirm their resiliency and to drop Travis CI.
    
    Signed-off-by: Sorin Sbarnea <ssbarnea@redhat.com>

---
+++ tools/test-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euxo pipefail
+# Used by Zuul CI to perform extra bootstrapping
+
+# Bumping system tox because version from CentOS 7 is too old
+# We are not using pip --user due to few bugs in tox role which does not allow
+# us to override how is called. Once these are addressed we will switch back
+# non-sudo mode.
+PYTHON=$(command -v python3 python | head -n1)
+
+sudo "$PYTHON" -m pip install -U tox "zipp<0.6.0;python_version=='2.7'"
