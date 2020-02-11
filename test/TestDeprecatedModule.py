e766eada379183b16cdc176b07b75e614a3a6231
Sviatoslav Sydorenko <webknjaz@redhat.com>
2020-02-11 16:19:37 +0100

    xfail test_module_deprecated under Python 3.8
    Ref: https://github.com/ansible/ansible-lint/issues/675

--- test/TestDeprecatedModule.py
+++ test/TestDeprecatedModule.py
@@ -1,8 +1,15 @@
+import sys
 import unittest
+
+import pytest
 
 from ansiblelint import RulesCollection
 from ansiblelint.rules.DeprecatedModuleRule import DeprecatedModuleRule
 from test import RunFromText
+
+
+IS_AT_LEAST_PY38 = sys.version_info[:2] >= (3, 8)
+
 
 MODULE_DEPRECATED = '''
 - name: task example
@@ -18,6 +25,10 @@ class TestDeprecatedModuleRule(unittest.TestCase):
     def setUp(self):
         self.runner = RunFromText(self.collection)
 
+    @pytest.mark.xfail(
+        IS_AT_LEAST_PY38, reason='',
+        raises=SystemExit, strict=True,
+    )
     def test_module_deprecated(self):
         results = self.runner.run_role_tasks_main(MODULE_DEPRECATED)
         self.assertEqual(1, len(results))
