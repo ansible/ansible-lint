e8f4fc36bfdb644214eaa6dad7703c33251644f8
Sorin Sbarnea <ssbarnea@redhat.com>
2020-03-16 11:05:51 +0000

    POC: fix zuul tox jobs
    Signed-off-by: Sorin Sbarnea <ssbarnea@redhat.com>

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -6,6 +6,15 @@ set -Eeuxo pipefail
 # We are not using pip --user due to few bugs in tox role which does not allow
 # us to override how is called. Once these are addressed we will switch back
 # non-sudo mode.
+sudo yum remove -y python-tox || true
+
 PYTHON=$(command -v python3 python | head -n1)
 
-sudo "$PYTHON" -m pip install -U tox "zipp<0.6.0;python_version=='2.7'"
+$PYTHON -m pip install --user "pip>=20.0.2"
+$PYTHON -m pip install --user "virtualenv>=20.0.10"
+$PYTHON -m pip install --user "tox>=3.8.0" "tox-venv" "zipp<0.6.0;python_version=='2.7'"
+
+# testing
+$PYTHON -m virtualenv --version
+$PYTHON -m tox --version
+tox --version
