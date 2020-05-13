6cbb21c4d2693fa7b65d364cf105bee4c01ca9fe
Sviatoslav Sydorenko <webknjaz@redhat.com>
2020-05-13 20:55:42 +0200

    Set default Sphinx role to "any"

--- docs/docsite/rst/conf.py
+++ docs/docsite/rst/conf.py
@@ -87,7 +87,7 @@ today_fmt = '%B %d, %Y'
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
-# default_role = None
+default_role = 'any'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 # add_function_parentheses = True
