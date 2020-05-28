db654fd5e66304c9caf313fc14fc5c45c4f436b1
Sorin Sbarnea <ssbarnea@redhat.com>
2020-05-28 18:34:23 +0100

    Enable stub/fake modules
    Provides a way to define a list of ansible modules that would be
    mocked by the linter in order to allow Ansible to load the linted
    files.
    
    This should fix a very common problem which hindered linting on
    various repositories, where the modules where hard to install inside
    the linting jobs.
    
    This changes also auto-adds `plugins/modules` if the folder is found,
    as this is the official location for putting modules.
    
    Fixes: #778

--- lib/ansiblelint/__main__.py
+++ lib/ansiblelint/__main__.py
@@ -20,22 +20,97 @@
 # THE SOFTWARE.
 """Command line implementation."""
 
+import collections
+from contextlib import contextmanager
 import errno
 import logging
 import pathlib
+import os
 import sys
-
-import ansiblelint.formatters as formatters
+import tempfile
 from ansiblelint import cli, default_rulesdir
-from ansiblelint.runner import Runner
-from ansiblelint.rules import RulesCollection
-from ansiblelint.utils import get_playbooks_and_roles
-from ansiblelint.utils import normpath, initialize_logger
 from ansiblelint.generate_docs import rules_as_rst
 from typing import Any, Set
 
 
 _logger = logging.getLogger(__name__)
+
+MODULE_STUB = """
+# This is a fake module used to make ansible(-lint) happy
+from ansible.module_utils.basic import AnsibleModule
+
+
+def main():
+    return AnsibleModule(
+        argument_spec=dict(
+            data=dict(default=None),
+            path=dict(default=None, type=str),
+            file=dict(default=None, type=str),
+        )
+    )
+"""
+
+
+def initialize_logger(level: int = 0) -> None:
+    """Set up the global logging level based on the verbosity number."""
+    VERBOSITY_MAP = {
+        0: logging.NOTSET,
+        1: logging.INFO,
+        2: logging.DEBUG
+    }
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(__package__)
+    logger.addHandler(handler)
+    # Unknown logging level is treated as DEBUG
+    logging_level = VERBOSITY_MAP.get(level, logging.DEBUG)
+    logger.setLevel(logging_level)
+    # Use module-level _logger instance to validate it
+    _logger.debug("Logging initialized to level %s", logging_level)
+
+
+@contextmanager
+def bootstrap_ansible(options):
+    """Perform Ansible boostraping steps to add custom module paths.
+
+    * Alter ANSIBLE_LIBRARY to inject custom paths before ansible modules are
+    loaded. See https://github.com/ansible/ansible/issues/69758
+    """
+    BootstrapContext = collections.namedtuple(
+        'BootstrapContext',
+        'change_module_path tmp_dir')
+    changed_module_path = False
+    tmp_dir = None
+
+    lib_paths = os.environ.get(
+        'ANSIBLE_LIBRARY',
+        "~/.ansible/plugins/modules:/usr/share/ansible/plugins/modules").split(":")
+    # add plugins/modules to module paths if it exists
+    p = pathlib.Path.cwd() / "plugins" / "modules"
+    if p.exists() and str(p) not in lib_paths:
+        _logger.info(f"Adding {str(p)} to module path")
+        lib_paths.append(str(p))
+        changed_module_path = True
+
+    if options.stub_modules:
+        # creates temporary directory for stub modules (auto-cleaned)
+        tmp_dir = tempfile.TemporaryDirectory()
+        lib_paths.append(tmp_dir.name)
+        _logger.info(f"Adding {tmp_dir.name} to module path")
+        changed_module_path = True
+
+        for m in options.stub_modules:
+            with (pathlib.Path(tmp_dir.name) / f"{m}.py") as f:
+                _logger.debug(f"Generating fake module stub: {f.name}")
+                f.write_text(MODULE_STUB)
+
+    if changed_module_path:
+        os.environ['ANSIBLE_LIBRARY'] = ":".join(lib_paths)
+        _logger.info(f"Altered module path ANSIBLE_LIBRARY={os.environ['ANSIBLE_LIBRARY']}")
+
+    yield BootstrapContext(changed_module_path, tmp_dir)
 
 
 def main():
@@ -47,64 +122,72 @@ def main():
     initialize_logger(options.verbosity)
     _logger.debug("Options: %s", options)
 
-    formatter_factory: Any = formatters.Formatter
-    if options.quiet:
-        formatter_factory = formatters.QuietFormatter
+    # assert 'ansible' not in sys.modules
+    with bootstrap_ansible(options):
 
-    if options.parseable:
-        formatter_factory = formatters.ParseableFormatter
+        from ansiblelint.utils import normpath, get_playbooks_and_roles
+        import ansiblelint.formatters as formatters
+        from ansiblelint.runner import Runner
+        from ansiblelint.rules import RulesCollection
 
-    if options.parseable_severity:
-        formatter_factory = formatters.ParseableSeverityFormatter
+        formatter_factory: Any = formatters.Formatter
+        if options.quiet:
+            formatter_factory = formatters.QuietFormatter
 
-    formatter = formatter_factory(cwd, options.display_relative_path)
+        if options.parseable:
+            formatter_factory = formatters.ParseableFormatter
 
-    if options.use_default_rules:
-        rulesdirs = options.rulesdir + [default_rulesdir]
-    else:
-        rulesdirs = options.rulesdir or [default_rulesdir]
-    rules = RulesCollection(rulesdirs)
+        if options.parseable_severity:
+            formatter_factory = formatters.ParseableSeverityFormatter
 
-    if options.listrules:
-        formatted_rules = rules if options.format == 'plain' else rules_as_rst(rules)
-        print(formatted_rules)
-        return 0
+        formatter = formatter_factory(cwd, options.display_relative_path)
 
-    if options.listtags:
-        print(rules.listtags())
-        return 0
+        if options.use_default_rules:
+            rulesdirs = options.rulesdir + [default_rulesdir]
+        else:
+            rulesdirs = options.rulesdir or [default_rulesdir]
+        rules = RulesCollection(rulesdirs)
 
-    if isinstance(options.tags, str):
-        options.tags = options.tags.split(',')
+        if options.listrules:
+            formatted_rules = rules if options.format == 'plain' else rules_as_rst(rules)
+            print(formatted_rules)
+            return 0
 
-    skip = set()
-    for s in options.skip_list:
-        skip.update(str(s).split(','))
-    options.skip_list = frozenset(skip)
+        if options.listtags:
+            print(rules.listtags())
+            return 0
 
-    if not options.playbook:
-        # no args triggers auto-detection mode
-        playbooks = get_playbooks_and_roles(options=options)
-    else:
-        playbooks = sorted(set(options.playbook))
+        if isinstance(options.tags, str):
+            options.tags = options.tags.split(',')
 
-    matches = list()
-    checked_files: Set[Any] = set()
-    for playbook in playbooks:
-        runner = Runner(rules, playbook, options.tags,
-                        options.skip_list, options.exclude_paths,
-                        options.verbosity, checked_files)
-        matches.extend(runner.run())
+        skip = set()
+        for s in options.skip_list:
+            skip.update(str(s).split(','))
+        options.skip_list = frozenset(skip)
 
-    matches.sort(key=lambda x: (normpath(x.filename), x.linenumber, x.rule.id))
+        if not options.playbook:
+            # no args triggers auto-detection mode
+            playbooks = get_playbooks_and_roles(options=options)
+        else:
+            playbooks = sorted(set(options.playbook))
 
-    for match in matches:
-        print(formatter.format(match, options.colored))
+        matches = list()
+        checked_files: Set[Any] = set()
+        for playbook in playbooks:
+            runner = Runner(rules, playbook, options.tags,
+                            options.skip_list, options.exclude_paths,
+                            options.verbosity, checked_files)
+            matches.extend(runner.run())
 
-    if len(matches):
-        return 2
-    else:
-        return 0
+        matches.sort(key=lambda x: (normpath(x.filename), x.linenumber, x.rule.id))
+
+        for match in matches:
+            print(formatter.format(match, options.colored))
+
+        if len(matches):
+            return 2
+        else:
+            return 0
 
 
 if __name__ == "__main__":
