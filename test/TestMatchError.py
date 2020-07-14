ansible/ansible-lint

commit 3a9249681f8bf35fa391d492a3cee102d3280181

Improve MatchError class (#881)
Co-authored-by: Sviatoslav Sydorenko <webknjaz@redhat.com>

lib/ansiblelint/errors.py
@@ -1,21 +1,39 @@
 """Exceptions and error representations."""
+import functools
+from ansiblelint.file_utils import normpath
 
 
+@functools.total_ordering
 class MatchError(ValueError):
     """Rule violation detected during linting.
 
     It can be raised as Exception but also just added to the list of found
     rules violations.
+
+    Note that line argument is not considered when building hash of an
+    instance.
     """
 
-    def __init__(self, message=None, linenumber=0, line=None, filename=None, rule=None) -> None:
+    def __init__(
+            self,
+            message=None,
+            linenumber=0,
+            line: dict = None,
+            filename=None,
+            rule=None) -> None:
         """Initialize a MatchError instance."""
         super().__init__(message)
+
+        if not (message or rule):
+            raise TypeError(
+                f'{self.__class__.__name__}() missing a '
+                "required argument: one of 'message' or 'rule'",
+            )
 
         self.message = message or getattr(rule, 'shortdesc', "")
         self.linenumber = linenumber
         self.line = line
-        self.filename = filename
+        self.filename = normpath(filename) if filename else None
         self.rule = rule
 
     def __repr__(self):
@@ -27,3 +45,24 @@ class MatchError(ValueError):
 
         return formatstr.format(_id, self.message,
                                 self.filename, self.linenumber, self.line)
+
+    @property
+    def _hash_key(self):
+        # line attr is knowingly excluded, as dict is not hashable
+        return self.filename, self.linenumber, str(getattr(self.rule, 'id', 0)), self.message
+
+    def __lt__(self, other):
+        """Return whether the current object is less than the other."""
+        if not isinstance(other, self.__class__):
+            raise NotImplementedError
+        return self._hash_key < other._hash_key
+
+    def __hash__(self):
+        """Return a hash value of the MatchError instance."""
+        return hash(self._hash_key)
+
+    def __eq__(self, other):
+        """Identify whether the other object represents the same rule match."""
+        if not isinstance(other, self.__class__):
+            raise NotImplementedError
+        return self.__hash__() == other.__hash__()

test/TestMatchError.py
@@ -1,0 +1,87 @@
+"""Tests for MatchError."""
+
+import operator
+
+from ansiblelint.errors import MatchError
+from ansiblelint.rules import AnsibleLintRule
+from ansiblelint.rules.AlwaysRunRule import AlwaysRunRule
+from ansiblelint.rules.BecomeUserWithoutBecomeRule import BecomeUserWithoutBecomeRule
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    ('left_match_error', 'right_match_error'),
+    (
+        (MatchError("foo"), MatchError("foo")),
+        # `line` is ignored in comparisions, even when it differs:
+        (MatchError("a", line={}), MatchError("a", line={"a": "b"})),
+    ),
+)
+def test_matcherror_compare(left_match_error, right_match_error):
+    """Check that MatchError instances with similar attrs are equivalent."""
+    assert left_match_error == right_match_error
+
+
+class AnsibleLintRuleWithStringId(AnsibleLintRule):
+    id = "ANSIBLE200"
+
+
+def test_matcherror_invalid():
+    """Ensure that MatchError requires message or rule."""
+    expected_err = r"^MatchError\(\) missing a required argument: one of 'message' or 'rule'$"
+    with pytest.raises(TypeError, match=expected_err):
+        MatchError()
+
+
+@pytest.mark.parametrize(
+    ('left_match_error', 'right_match_error'), (
+        # sorting by message
+        (MatchError("z"), MatchError("a")),
+        # filenames takes priority in sorting
+        (MatchError("a", filename="b"), MatchError("a", filename="a")),
+        # rule id 501 > rule id 101
+        (MatchError(rule=BecomeUserWithoutBecomeRule), MatchError(rule=AlwaysRunRule)),
+        # rule id "200" > rule id 101
+        (MatchError(rule=AnsibleLintRuleWithStringId), MatchError(rule=AlwaysRunRule)),
+        # line will not be taken into account
+        (MatchError("b", line={}), MatchError("a", line={"a": "b"})),
+    ))
+class TestMatchErrorCompare:
+
+    def test_match_error_less_than(self, left_match_error, right_match_error):
+        """Check 'less than' protocol implementation in MatchError."""
+        assert right_match_error < left_match_error
+
+    def test_match_error_greater_than(self, left_match_error, right_match_error):
+        """Check 'greater than' protocol implementation in MatchError."""
+        assert left_match_error > right_match_error
+
+    def test_match_error_not_equal(self, left_match_error, right_match_error):
+        """Check 'not equals' protocol implementation in MatchError."""
+        assert left_match_error != right_match_error
+
+
+@pytest.mark.parametrize(
+    'other',
+    (
+        None,
+        "foo",
+        42,
+        Exception("foo"),
+    ),
+)
+@pytest.mark.parametrize(
+    'operation',
+    (
+        operator.eq,
+        operator.ne,
+        operator.le,
+        operator.gt,
+    ),
+    ids=['eq', 'ne', 'le', 'gt']
+)
+def test_matcherror_compare_invalid(other, operation):
+    """Check that MatchError comparison with other types raises."""
+    with pytest.raises(NotImplementedError):
+        operation(MatchError("foo"), other)

lib/ansiblelint/__main__.py
@@ -29,7 +29,7 @@ from typing import Any, Set
 from ansiblelint import cli
 from ansiblelint.constants import DEFAULT_RULESDIR
 from ansiblelint.generate_docs import rules_as_rst
-from ansiblelint.utils import normpath, get_playbooks_and_roles
+from ansiblelint.utils import get_playbooks_and_roles
 import ansiblelint.formatters as formatters
 from ansiblelint.runner import Runner
 from ansiblelint.rules import RulesCollection
@@ -115,13 +115,7 @@ def main() -> int:
                         options.verbosity, checked_files)
         matches.extend(runner.run())
 
-    matches.sort(
-        key=lambda x: (
-            normpath(x.filename),
-            x.linenumber,
-            getattr(x.rule, 'id', 0)))
-
-    for match in matches:
+    for match in sorted(matches):
         print(formatter.format(match, options.colored))
 
     if len(matches):

commit 6d1aa10d884f681ad5b921ef147fb00e679c5fc5

Fix MatchError comparison fallback implementation
The previous code wrongly raised NotImplementedError but the
correct way is to return the NotImplemented singleton instead.

This is a bugfix for the broken implementation introduced in #881.

Ref: https://docs.python.org/3/reference/datamodel.html#object.__lt__

lib/ansiblelint/errors.py
@@ -14,6 +14,11 @@ class MatchError(ValueError):
     instance.
     """
 
+    # IMPORTANT: any additional comparison protocol methods must return
+    # IMPORTANT: `NotImplemented` singleton to allow the check to use the
+    # IMPORTANT: other object's fallbacks.
+    # Ref: https://docs.python.org/3/reference/datamodel.html#object.__lt__
+
     def __init__(
             self,
             message=None,
@@ -54,7 +59,7 @@ class MatchError(ValueError):
     def __lt__(self, other):
         """Return whether the current object is less than the other."""
         if not isinstance(other, self.__class__):
-            raise NotImplementedError
+            return NotImplemented
         return self._hash_key < other._hash_key
 
     def __hash__(self):
@@ -64,5 +69,5 @@ class MatchError(ValueError):
     def __eq__(self, other):
         """Identify whether the other object represents the same rule match."""
         if not isinstance(other, self.__class__):
-            raise NotImplementedError
+            return NotImplemented
         return self.__hash__() == other.__hash__()

test/TestMatchError.py
@@ -10,6 +10,42 @@ from ansiblelint.rules.BecomeUserWithoutBecomeRule import BecomeUserWithoutBecom
 import pytest
 
 
+class DummyTestObject:
+    """A dummy object for equality tests."""
+
+    def __repr__(self):
+        """Return a dummy object representation for parmetrize."""
+        return '{self.__class__.__name__}()'.format(self=self)
+
+    def __eq__(self, other):
+        """Report the equality check failure with any object."""
+        return False
+
+    def __ne__(self, other):
+        """Report the confirmation of inequality with any object."""
+        return True
+
+
+class DummySentinelTestObject:
+    """A dummy object for equality protocol tests with sentinel."""
+
+    def __eq__(self, other):
+        """Return sentinel as result of equality check w/ anything."""
+        return 'EQ_SENTINEL'
+
+    def __ne__(self, other):
+        """Return sentinel as result of inequality check w/ anything."""
+        return 'NE_SENTINEL'
+
+    def __lt__(self, other):
+        """Return sentinel as result of less than check w/ anything."""
+        return 'LT_SENTINEL'
+
+    def __gt__(self, other):
+        """Return sentinel as result of greater than chk w/ anything."""
+        return 'GT_SENTINEL'
+
+
 @pytest.mark.parametrize(
     ('left_match_error', 'right_match_error'),
     (
@@ -70,18 +106,74 @@ class TestMatchErrorCompare:
         42,
         Exception("foo"),
     ),
+    ids=repr,
 )
 @pytest.mark.parametrize(
-    'operation',
+    ('operation', 'operator_char'),
     (
-        operator.eq,
-        operator.ne,
-        operator.le,
-        operator.gt,
+        pytest.param(operator.le, '<=', id='<='),
+        pytest.param(operator.gt, '>', id='>'),
     ),
-    ids=['eq', 'ne', 'le', 'gt']
 )
-def test_matcherror_compare_invalid(other, operation):
-    """Check that MatchError comparison with other types raises."""
-    with pytest.raises(NotImplementedError):
+def test_matcherror_compare_no_other_fallback(other, operation, operator_char):
+    """Check that MatchError comparison with other types causes TypeError."""
+    expected_error = (
+        r'^('
+        r'unsupported operand type\(s\) for {operator!s}:|'
+        r"'{operator!s}' not supported between instances of"
+        r") 'MatchError' and '{other_type!s}'$".
+        format(other_type=type(other).__name__, operator=operator_char)
+    )
+    with pytest.raises(TypeError, match=expected_error):
         operation(MatchError("foo"), other)
+
+
+@pytest.mark.parametrize(
+    'other',
+    (
+        None,
+        'foo',
+        42,
+        Exception('foo'),
+        DummyTestObject(),
+    ),
+    ids=repr,
+)
+@pytest.mark.parametrize(
+    ('operation', 'expected_value'),
+    (
+        (operator.eq, False),
+        (operator.ne, True),
+    ),
+    ids=('==', '!=')
+)
+def test_matcherror_compare_with_other_fallback(
+        other,
+        operation,
+        expected_value,
+):
+    """Check that MatchError comparison runs other types fallbacks."""
+    assert operation(MatchError("foo"), other) is expected_value
+
+
+@pytest.mark.parametrize(
+    ('operation', 'expected_value'),
+    (
+        (operator.eq, 'EQ_SENTINEL'),
+        (operator.ne, 'NE_SENTINEL'),
+        # NOTE: these are swapped because when we do `x < y`, and `x.__lt__(y)`
+        # NOTE: returns `NotImplemented`, Python will reverse the check into
+        # NOTE: `y > x`, and so `y.__gt__(x) is called.
+        # Ref: https://docs.python.org/3/reference/datamodel.html#object.__lt__
+        (operator.lt, 'GT_SENTINEL'),
+        (operator.gt, 'LT_SENTINEL'),
+    ),
+    ids=('==', '!=', '<', '>'),
+)
+def test_matcherror_compare_with_dummy_sentinel(operation, expected_value):
+    """Check that MatchError comparison runs other types fallbacks."""
+    dummy_obj = DummySentinelTestObject()
+    # NOTE: This assertion abuses the CPython property to cache short string
+    # NOTE: objects because the identity check is more presice and we don't
+    # NOTE: want extra operator protocol methods to influence the test.
+    assert operation(MatchError("foo"), dummy_obj) is expected_value