c2cb0a31cfd8e01e9964f098fe27c444bd48ffa1
Sorin Sbarnea <ssbarnea@users.noreply.github.com>
2020-08-12 13:37:09 +0100

    Added MissingFilePermissionsRule (#943)
    Makes missing file permissions a linting error in order to proactively detect security issues that were fixed via
    https://github.com/ansible/ansible/issues/67794 at the expense of breaking usage that relied on default settings.

--- test/conftest.py
+++ test/conftest.py
@@ -19,4 +19,13 @@ def default_text_runner(default_rules_collection):
     return RunFromText(default_rules_collection)
 
 
+@pytest.fixture
+def rule_runner(request):
+    """Return runner for a specific rule class."""
+    rule_class = request.param
+    collection = RulesCollection()
+    collection.register(rule_class())
+    return RunFromText(collection)
+
+
 # vim: et:sw=4:syntax=python:ts=4:
