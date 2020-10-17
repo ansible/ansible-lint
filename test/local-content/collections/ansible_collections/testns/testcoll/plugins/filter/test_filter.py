bbb3333313b8edd0a019f9785d98dfedc446518b
Felix Fontein <felix@fontein.de>
2020-10-17 16:30:40 +0200

    Adding tests.

---
+++ test/local-content/collections/ansible_collections/testns/testcoll/plugins/filter/test_filter.py
@@ -0,0 +1,16 @@
+"""A filter plugin."""
+
+
+def a_test_filter(a, b):
+    """Return a string containing both a and b."""
+    return '{0}:{1}'.format(a, b)
+
+
+class FilterModule(object):
+    """Filter plugin."""
+
+    def filters(self):
+        """Return filters."""
+        return {
+            'test_filter': a_test_filter
+        }
---
+++ test/local-content/test-roles-failed-complete/roles/role2/test_plugins/b_failed_complete.py
@@ -0,0 +1,16 @@
+"""A test plugin."""
+
+
+def compatibility_in_test(a, b):
+    """Return True when a is contained in b."""
+    return a in b
+
+
+class TestModule:
+    """Test plugin."""
+
+    def tests(self):
+        """Return tests."""
+        return {
+            'b_test_failed_complete': compatibility_in_test,
+        }
