3f4caf5936d1bc3f287e3e698ba2c35606a3c56d
Sorin Sbarnea <ssbarnea@redhat.com>
2020-12-30 13:30:31 +0000

    More type hints

--- lib/ansiblelint/runner.py
+++ lib/ansiblelint/runner.py
@@ -36,7 +36,7 @@ class Runner(object):
             skip_list: Optional[FrozenSet[Any]] = frozenset(),
             exclude_paths: List[str] = [],
             verbosity: int = 0,
-            checked_files: Set[str] = None) -> None:
+            checked_files: Optional[Set[str]] = None) -> None:
         """Initialize a Runner instance."""
         self.rules = rules
         self.playbooks = set()
--- test/TestWithSkipTagId.py
+++ test/TestWithSkipTagId.py
@@ -11,29 +11,41 @@ class TestWithSkipTagId(unittest.TestCase):
     collection.register(TrailingWhitespaceRule())
     file = 'test/with-skip-tag-id.yml'
 
-    def test_negative_no_param(self):
-        bad_runner = Runner(self.collection, self.file, [], [], [])
+    def test_negative_no_param(self) -> None:
+        bad_runner = Runner(rules=self.collection, playbook=self.file)
         errs = bad_runner.run()
         self.assertGreater(len(errs), 0)
 
-    def test_negative_with_id(self):
+    def test_negative_with_id(self) -> None:
         with_id = '201'
-        bad_runner = Runner(self.collection, self.file, [with_id], [], [])
+        bad_runner = Runner(
+            rules=self.collection,
+            playbook=self.file,
+            tags=frozenset([with_id]))
         errs = bad_runner.run()
         self.assertGreater(len(errs), 0)
 
-    def test_negative_with_tag(self):
+    def test_negative_with_tag(self) -> None:
         with_tag = 'formatting'
-        bad_runner = Runner(self.collection, self.file, [with_tag], [], [])
+        bad_runner = Runner(
+            rules=self.collection,
+            playbook=self.file,
+            tags=frozenset([with_tag]))
         errs = bad_runner.run()
         self.assertGreater(len(errs), 0)
 
-    def test_positive_skip_id(self):
+    def test_positive_skip_id(self) -> None:
         skip_id = '201'
-        good_runner = Runner(self.collection, self.file, [], [skip_id], [])
+        good_runner = Runner(
+            rules=self.collection,
+            playbook=self.file,
+            skip_list=frozenset([skip_id]))
         self.assertEqual([], good_runner.run())
 
-    def test_positive_skip_tag(self):
+    def test_positive_skip_tag(self) -> None:
         skip_tag = 'formatting'
-        good_runner = Runner(self.collection, self.file, [], [skip_tag], [])
+        good_runner = Runner(
+            rules=self.collection,
+            playbook=self.file,
+            skip_list=frozenset([skip_tag]))
         self.assertEqual([], good_runner.run())
