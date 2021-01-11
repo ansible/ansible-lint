9d1ed14c5c207be4511c62da60552ab7a1ed246b
Sorin Sbarnea <ssbarnea@redhat.com>
2021-01-11 12:54:14 +0000

    Auto-detect modules from collection layouts (#1226)
    Makes local modules available to ansible when the plugins/modules exists
    on disk but only if the user did not already define ANSIBLE_LIBRARY.
    
    This means that from now on users are not forced to use use the old
    hack of defining ANSIBLE_LIBRARY=plugins/modules before running the
    linter, something that made it harder to use with pre-commit, tox, CI.
    
    If the variable is already defined or the folder does not exist,
    nothing is done.
    
    Fixes: #778

---
+++ plugins/modules/fake_module.py
@@ -0,0 +1,16 @@
+"""Sample custom ansible module named fake_module.
+
+This is used to test ability to detect and use custom modules.
+"""
+from ansible.module_utils.basic import AnsibleModule
+
+
+def main():
+    """Return the module instance."""
+    return AnsibleModule(
+        argument_spec=dict(
+            data=dict(default=None),
+            path=dict(default=None, type=str),
+            file=dict(default=None, type=str),
+        )
+    )
