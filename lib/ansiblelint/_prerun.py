833c505bf70877ca43a49867881d313ce4a2fb4e
Sorin Sbarnea <ssbarnea@redhat.com>
2021-01-14 15:50:23 +0000

    Auto install requirements into temp directory
    If `requirements.yml` is found, install roles and collections from
    it into a temporary folder in order to avoid runtime errors when
    ansible would not be able to find roles or modules.
    
    Fixes: #930

--- lib/ansiblelint/_prerun.py
+++ lib/ansiblelint/_prerun.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 
 from packaging import version
@@ -38,6 +39,57 @@ def prepare_environment() -> None:
     if os.path.exists("roles") and "ANSIBLE_ROLES_PATH" not in os.environ:
         os.environ['ANSIBLE_ROLES_PATH'] = "roles"
         print("Added ANSIBLE_ROLES_PATH=roles", file=sys.stderr)
+
+    if os.path.exists("requirements.yml"):
+
+        cmd = [
+            "ansible-galaxy",
+            "install",
+            "--roles-path",
+            ".cache/roles",
+            "-vr",
+            "requirements.yml"
+            ]
+
+        print("Running %s" % " ".join(cmd))
+        run = subprocess.run(
+            cmd,
+            universal_newlines=True,
+            check=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if run.returncode != 0:
+            sys.exit(run.returncode)
+
+        cmd = [
+            "ansible-galaxy",
+            "collection",
+            "install",
+            "-p",
+            ".cache/collections",
+            "-vr",
+            "requirements.yml"
+            ]
+
+        print("Running %s" % " ".join(cmd))
+        run = subprocess.run(
+            cmd,
+            universal_newlines=True,
+            check=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if run.returncode != 0:
+            sys.exit(run.returncode)
+
+        if 'ANSIBLE_ROLES_PATH' in os.environ:
+            os.environ['ANSIBLE_ROLES_PATH'] = f".cache/roles:{os.environ['ANSIBLE_ROLES_PATH']}"
+        if 'ANSIBLE_COLLECTIONS_PATHS' in os.environ:
+            os.environ['ANSIBLE_COLLECTIONS_PATHS'] = \
+                f".cache/collections:{os.environ['ANSIBLE_COLLECTIONS_PATHS']}"
+        else:
+            os.environ['ANSIBLE_COLLECTIONS_PATHS'] = ".cache/collections"
 
 
 check_ansible_presence()
