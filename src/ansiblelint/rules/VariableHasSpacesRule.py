720e4032cc0be2ba09f9534f9afa98ad73dc7ed0
Sorin Sbarnea <ssbarnea@redhat.com>
2021-02-01 13:20:01 +0000

    E206: Make us of matchtask
    This also fixes issue which prevented use of noqa, a known limitation
    of old match.

--- src/ansiblelint/rules/VariableHasSpacesRule.py
+++ src/ansiblelint/rules/VariableHasSpacesRule.py
@@ -3,8 +3,10 @@
 
 import re
 import sys
+from typing import Any, Dict, Union
 
 from ansiblelint.rules import AnsibleLintRule
+from ansiblelint.utils import nested_items
 
 
 class VariableHasSpacesRule(AnsibleLintRule):
@@ -18,11 +20,13 @@ class VariableHasSpacesRule(AnsibleLintRule):
     variable_syntax = re.compile(r"{{.*}}")
     bracket_regex = re.compile(r"{{[^{' -]|[^ '}-]}}")
 
-    def match(self, line: str) -> bool:
-        if not self.variable_syntax.search(line):
-            return False
-        line_exclude_json = re.sub(r"[^{]{'\w+': ?[^{]{.*?}}", "", line)
-        return bool(self.bracket_regex.search(line_exclude_json))
+    def matchtask(self, task: Dict[str, Any]) -> Union[bool, str]:
+        for k, v in nested_items(task):
+            if isinstance(v, str):
+                line_exclude_json = re.sub(r"[^{]{'\w+': ?[^{]{.*?}}", "", v)
+                if bool(self.bracket_regex.search(line_exclude_json)):
+                    return True
+        return False
 
 
 if 'pytest' in sys.modules:
