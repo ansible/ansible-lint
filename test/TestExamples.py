4a1b96cdecb5c5ee1d221d5424dd4a2831bb9226
Felix Fontein <felix@fontein.de>
2021-02-14 10:37:20 +0100

    Ensure that yamllint config ignore entries are used (#1346)
    * Ensure that yamllint config ignore entries are used.
    
    * Linting.
    
    * Change into examples/ for test.
    
    This ensures that the top-level .yamllint config does not prevent some of the
    errors to be reported in this file.
    
    * More linting.

--- test/TestExamples.py
+++ test/TestExamples.py
@@ -1,14 +1,22 @@
 """Assure samples produced desire outcomes."""
+import os
+
 import pytest
 
 from ansiblelint.runner import Runner
 
 
+@pytest.fixture
+def _change_into_examples_dir(request):
+    os.chdir('examples')
+    yield
+    os.chdir('..')
+
+
+@pytest.mark.usefixtures('_change_into_examples_dir')
 def test_example(default_rules_collection):
     """example.yml is expected to have 15 match errors inside."""
-    result = Runner(
-        'examples/playbooks/example.yml', rules=default_rules_collection
-    ).run()
+    result = Runner('playbooks/example.yml', rules=default_rules_collection).run()
     assert len(result) == 15
 
 
