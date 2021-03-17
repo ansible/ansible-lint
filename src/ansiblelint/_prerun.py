f79a37da47ddf1c99ac0ee92a0cf6f6c186525a9
Sorin Sbarnea <ssbarnea@redhat.com>
2021-03-17 15:16:40 +0000

    Fixed matching patterns used for mocking

--- src/ansiblelint/_prerun.py
+++ src/ansiblelint/_prerun.py
@@ -226,25 +226,28 @@ def _prepare_ansible_paths() -> None:
 
 def _make_module_stub(module_name: str) -> None:
     # a.b.c is treated a collection
-    if re.match(r"\w+\.\w+\.\w+", module_name):
-        namespace, collection, module_file = module_name.split(".")
-        path = f"{ options.project_dir }/.cache/collections/ansible_collections/{ namespace }/{ collection }/plugins/modules"
+    if re.match(r"^(\w+|\w+\.\w+\.[\.\w]+)$", module_name):
+        parts = module_name.split(".")
+        if len(parts) < 3:
+            path = f"{options.project_dir}/.cache/modules"
+            module_file = f"{options.project_dir}/.cache/modules/{module_name}.py"
+            namespace = None
+            collection = None
+        else:
+            namespace = parts[0]
+            collection = parts[1]
+            path = f"{ options.project_dir }/.cache/collections/ansible_collections/{ namespace }/{ collection }/plugins/modules/{ '/'.join(parts[2:-1]) }"
+            module_file = f"{path}/{parts[-1]}.py"
         os.makedirs(path, exist_ok=True)
         _write_module_stub(
-            filename=f"{path}/{module_file}.py",
+            filename=module_file,
             name=module_file,
             namespace=namespace,
             collection=collection,
         )
-    elif "." in module_name:
+    else:
         _logger.error("Config error: %s is not a valid module name.", module_name)
         sys.exit(INVALID_CONFIG_RC)
-    else:
-        os.makedirs(f"{options.project_dir}/.cache/modules", exist_ok=True)
-        _write_module_stub(
-            filename=f"{options.project_dir}/.cache/modules/{module_name}.py",
-            name=module_name,
-        )
 
 
 def _write_module_stub(
@@ -274,7 +277,7 @@ def _update_env(varname: str, value: List[str], default: str = "") -> None:
 def _perform_mockings() -> None:
     """Mock modules and roles."""
     for role_name in options.mock_roles:
-        if re.match(r"\w+\.\w+\.\w+", role_name):
+        if re.match(r"\w+\.\w+\.\w+$", role_name):
             namespace, collection, role_dir = role_name.split(".")
             path = f".cache/collections/ansible_collections/{ namespace }/{ collection }/roles/{ role_dir }/"
         else:
