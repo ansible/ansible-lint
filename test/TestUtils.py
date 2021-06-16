70fb5161bb340bc56cec8dab6512aa86b6a868d5
Sorin Sbarnea <ssbarnea@redhat.com>
2021-06-16 11:39:20 +0100

    Black fixes

--- test/TestUtils.py
+++ test/TestUtils.py
@@ -191,7 +191,13 @@ def test_template_lookup(role: str, expect_warning: bool) -> None:
     """Assure lookup plugins used in templates does not trigger Ansible warnings."""
     task_path = os.path.realpath(
         os.path.join(
-            os.path.dirname(os.path.realpath(__file__)), "..", "examples", "roles", role, "tasks", "main.yml"
+            os.path.dirname(os.path.realpath(__file__)),
+            "..",
+            "examples",
+            "roles",
+            role,
+            "tasks",
+            "main.yml",
         )
     )
     result = run_ansible_lint("-v", task_path)
