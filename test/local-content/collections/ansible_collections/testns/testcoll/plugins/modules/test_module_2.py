23f7d94ce7a5918d9641d74cd19511a0fb950bc3
Nick Smith <clickthisnick@users.noreply.github.com>
2021-07-14 12:50:04 -0400

    Update test_module_2.py

--- test/local-content/collections/ansible_collections/testns/testcoll/plugins/modules/test_module_2.py
+++ test/local-content/collections/ansible_collections/testns/testcoll/plugins/modules/test_module_2.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
+
+# Switching the pre-commit repo from the deprecated pylint-mirrors to pylint caused this pylint issue to surface.
+# The disable can be removed if it is a false positive which will be fixed in upstream pylint or the cyclic-import is fixed such that pylint no longer complains
 # pylint: disable=cyclic-import
+
 """A module."""
 
 from ansible.module_utils.basic import AnsibleModule
