5e417e50a7d74dd8a2026a4e221dfedcf0b663e8
Jacob Floyd <cognifloyd@gmail.com>
2021-11-04 18:37:17 -0500

    make Transformer write YAML files

--- src/ansiblelint/transformer.py
+++ src/ansiblelint/transformer.py
@@ -1,10 +1,13 @@
 """Transformer implementation."""
 import logging
-from typing import List, Set
+from typing import Dict, List, Set, Union
+
+from ruamel.yaml.comments import CommentedSeq, CommentedMap
 
 from ansiblelint.errors import MatchError
 from ansiblelint.file_utils import Lintable
 from ansiblelint.runner import LintResult
+from ansiblelint.yaml_utils import FormattedYAML
 
 __all__ = ["Transformer"]
 
@@ -24,5 +27,26 @@ class Transformer:
         self.matches: List[MatchError] = result.matches
         self.files: Set[Lintable] = result.files
 
+        file: Lintable
+        lintables: Dict[str, Lintable] = {file.filename: file for file in result.files}
+        self.matches_per_file: Dict[Lintable, List[MatchError]] = {
+            file: [] for file in result.files
+        }
+
+        for match in self.matches:
+            try:
+                lintable = lintables[match.filename]
+            except KeyError:
+                # we shouldn't get here, but this is easy to recover from so do that.
+                lintable = Lintable(match.filename)
+                self.matches_per_file[lintable] = []
+            self.matches_per_file[lintable].append(match)
+
     def run(self) -> None:
         """For each file, read it, execute transforms on it, then write it."""
+        yaml = FormattedYAML()
+        for file, matches in self.matches_per_file.items():
+            if file.base_kind == "text/yaml":
+                ruamel_data: Union[CommentedMap, CommentedSeq] = yaml.loads(file.content)
+                file.content = yaml.dumps(ruamel_data)
+                file.write()
