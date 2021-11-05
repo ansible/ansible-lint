78fdb3a341e8947d912a73ba956d3741a09115ee
Jacob Floyd <cognifloyd@gmail.com>
2021-11-04 19:28:22 -0500

    Add --fmt option to enable transforming yaml

--- src/ansiblelint/cli.py
+++ src/ansiblelint/cli.py
@@ -197,6 +197,9 @@ def get_cli_parser() -> argparse.ArgumentParser:
         help="Keep default rules when using -r",
     )
     parser.add_argument(
+        "--fmt", dest="do_transforms", action="store_true", help="Reformat/Transform files to fix issues where possible"
+    )
+    parser.add_argument(
         "--show-relpath",
         dest="display_relative_path",
         action="store_false",
