5cd9bdf486ef12efca53295f55cfc318ba7f6596
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-11-11 04:32:19 +0100

    Set a logo in the Sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -231,12 +231,12 @@ html_title = 'Ansible Lint Documentation'
 
 # The name of an image file (within the static path) to place at the top of
 # the sidebar.
-# html_logo = None
+html_logo = '_static/ansible-lint.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '_static/ansible-lint.svg'
+html_favicon = html_logo
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
