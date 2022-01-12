aa589e58f0fe008b0354606df8be3d51f8937cd4
Jacob Floyd <cognifloyd@gmail.com>
2022-01-12 16:29:52 -0600

    Make sure to visit all child nodes in Jinja ast

--- src/ansiblelint/transforms/SimplifyLiteralBoolComparisonTransform.py
+++ src/ansiblelint/transforms/SimplifyLiteralBoolComparisonTransform.py
@@ -24,13 +24,13 @@ class CompareNodeTransformer(NodeTransformer):
         if len(node.ops) > 1:
             # How do I transform a Compare with multiple Operands?
             # That does not make sense with !=,== bool, so just skip it.
-            return node
+            return self.generic_visit(node)
 
         operand: nodes.Operand = node.ops[0]
         op: str = operand.op
         right: nodes.Expr = operand.expr
         if not isinstance(right, nodes.Const):
-            return node
+            return self.generic_visit(node)
 
         exp = (op, right.value)
         if exp == (TOKEN_EQ, True) or exp == (TOKEN_NE, False):
@@ -40,12 +40,12 @@ class CompareNodeTransformer(NodeTransformer):
             # not var
             negate = True
         else:
-            return node
+            return self.generic_visit(node)
 
         if negate:
-            return nodes.Not(left, lineno=node.lineno)
+            return self.generic_visit(nodes.Not(left, lineno=node.lineno))
         else:
-            return left
+            return self.generic_visit(left)
 
 
 class SimplifyLiteralBoolComparisonTransform(Transform):
