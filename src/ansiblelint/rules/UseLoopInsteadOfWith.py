24bed3163b6e57542f26172ba8a57d37a91dabc7
Jacob Floyd <cognifloyd@gmail.com>
2022-01-17 11:41:30 -0600

    Add UseLoopInsteadOfWith rule

---
+++ src/ansiblelint/rules/UseLoopInsteadOfWith.py
@@ -0,0 +1,23 @@
+from typing import Any, Dict, Optional, Union
+
+from ansiblelint.file_utils import Lintable
+from ansiblelint.rules import AnsibleLintRule
+
+
+class UseLoopInsteadOfWith(AnsibleLintRule):
+    id = "no-with-loops"
+    shortdesc = "Use loop instead of with_* style loops."
+    description = shortdesc + (
+        " The `with_*` style loops make it difficult to use automated "
+        "tooling, like schemas, to validate playbooks."
+    )
+    severity = "LOW"
+    tags = ["deprecations", "opt-in"]
+    version_added = "5.3"
+
+    def matchtask(
+        self, task: Dict[str, Any], file: Optional[Lintable] = None
+    ) -> Union[bool, str]:
+        with_keys = [key for key in task if key.startswith("with_")]
+        has_with_style_loop = bool(with_keys)
+        return has_with_style_loop
