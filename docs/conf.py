1ba64c33bef82b2732e1b5cfa9ce029dd81d5fe9
Jacob Floyd <cognifloyd@gmail.com>
2022-01-19 10:58:26 -0600

    tell sphinx to ignore some classes

--- docs/conf.py
+++ docs/conf.py
@@ -293,6 +293,10 @@ nitpick_ignore = [
     ("py:class", "Pattern"),
     ("py:class", "odict"),
     ("py:class", "LintResult"),
+    ("py:class", "ruamel.yaml.comments.CommentedMap"),
+    ("py:class", "ruamel.yaml.comments.CommentedSeq"),
+    ("py:class", "CommentedMap"),
+    ("py:class", "CommentedSeq"),
     ("py:obj", "Any"),
     ("py:obj", "ansiblelint.formatters.T"),
 ]
