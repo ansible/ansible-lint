89a3d75c8f209bfbc52cc239ee6364d809c658ac
Jacob Floyd <cognifloyd@gmail.com>
2022-01-19 16:10:03 -0600

    Add basic no-with-loops rule tests

--- src/ansiblelint/rules/UseLoopInsteadOfWith.py
+++ src/ansiblelint/rules/UseLoopInsteadOfWith.py
@@ -1,4 +1,5 @@
-from typing import Any, Dict, Optional, Union
+import sys
+from typing import Any, Dict, List, Optional, Union
 
 from ansiblelint.file_utils import Lintable
 from ansiblelint.rules import AnsibleLintRule
@@ -21,3 +22,66 @@ class UseLoopInsteadOfWith(AnsibleLintRule):
         with_keys = [key for key in task if key.startswith("with_")]
         has_with_style_loop = bool(with_keys)
         return has_with_style_loop
+
+
+if 'pytest' in sys.modules:
+
+    import pytest
+
+    from ansiblelint.testing import RunFromText  # pylint: disable=ungrouped-imports
+
+    LOOP = '''
+- hosts: all
+  tasks:
+    - name: Pass when loop is used
+      debug:
+        msg: "{{ item }}"
+      loop:
+        - hello
+        - world
+'''
+
+    WITH_ITEMS = '''
+- hosts: all
+  tasks:
+    - name: Fail when with_items is used
+      debug:
+        msg: "{{ item }}"
+      with_items:
+        - hello
+        - world
+'''
+
+    WITH_INDEXED_ITEMS = '''
+- hosts: all
+  tasks:
+    - name: Fail when with_indexed_items is used
+      debug:
+        msg: "{{ item.0 }} - {{ item.1 }}"
+      with_indexed_items: "{{ items }}"
+'''
+
+
+    @pytest.mark.parametrize(
+        'rule_runner', (UseLoopInsteadOfWith,), indirect=['rule_runner']
+    )
+    def test_loop(rule_runner: RunFromText) -> None:
+        """The task uses 'loop' to loop."""
+        results = rule_runner.run_playbook(LOOP)
+        assert len(results) == 0
+
+    @pytest.mark.parametrize(
+        'rule_runner', (UseLoopInsteadOfWith,), indirect=['rule_runner']
+    )
+    def test_no_with_items(rule_runner: RunFromText) -> None:
+        """The task uses 'with_items' to loop."""
+        results = rule_runner.run_playbook(WITH_ITEMS)
+        assert len(results) == 1
+
+    @pytest.mark.parametrize(
+        'rule_runner', (UseLoopInsteadOfWith,), indirect=['rule_runner']
+    )
+    def test_no_with_indexed_items(rule_runner: RunFromText) -> None:
+        """The task uses 'with_indexed_items' to loop."""
+        results = rule_runner.run_playbook(WITH_INDEXED_ITEMS)
+        assert len(results) == 1
