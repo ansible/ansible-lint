dfa706836c3e95086c58e62a87c6e6751e355429
Jacob Floyd <cognifloyd@gmail.com>
2022-01-19 20:56:00 -0600

    check point with-loop transforms

--- src/ansiblelint/rules/UseLoopInsteadOfWith.py
+++ src/ansiblelint/rules/UseLoopInsteadOfWith.py
@@ -1,6 +1,7 @@
+import functools
 import os
 import sys
-from typing import Any, Dict, List, Optional, Union, cast
+from typing import Any, Dict, List, MutableMapping, Optional, Union, cast
 
 from ansible.template import Templar
 from jinja2 import nodes
@@ -13,6 +14,23 @@ from ansiblelint.file_utils import Lintable
 from ansiblelint.rules import AnsibleLintRule, TransformMixin
 from ansiblelint.transform_utils import dump
 from ansiblelint.utils import LINE_NUMBER_KEY, ansible_templar
+
+
+def filter_with_flatten(
+    jinja_env: Environment, loop_value: str, levels: int = None
+) -> Optional[str]:
+    loop_ast = jinja_env.parse(loop_value)
+    output_node = cast(nodes.Output, loop_ast.body[0])
+    if len(output_node.nodes) != 1:
+        # unexpected template.
+        # There shouldn't be TemplateData nodes or more than one expression.
+        return None
+    node: nodes.Node = output_node.nodes[0]
+    kwargs = []
+    if levels is not None:
+        kwargs.append(nodes.Keyword("levels", nodes.Const(1)))
+    output_node.nodes[0] = nodes.Filter(node, "flatten", [], kwargs, None, None)
+    return dump(node=loop_ast, environment=jinja_env)
 
 
 # noinspection PyMethodMayBeStatic
@@ -61,15 +79,29 @@ class UseLoopInsteadOfWith(AnsibleLintRule, TransformMixin):
         if fixed:
             self._fixed(match)
 
-    def _set_loop_value(
-        self, loop_type: str, loop_value: Any, target_task: CommentedMap
-    ):
+    @staticmethod
+    def _set_loop_value(loop_type: str, loop_value: Any, target_task: CommentedMap):
         position = list(target_task.keys()).index(loop_type)
         target_task.insert(position, "loop", loop_value)
         comment = target_task.ca.items.pop(loop_type, None)
         if comment is not None:
             target_task.ca.items["loop"] = comment
         del target_task[loop_type]
+
+    @staticmethod
+    def _set_vars(vars_: Optional[CommentedMap], target_task: CommentedMap):
+        if vars_ is None:
+            return
+        comment = target_task.ca.items.pop("vars", None)
+        target_task["vars"] = vars_
+        if comment is not None:
+            target_task.ca.items["vars"] = comment
+
+    @staticmethod
+    def _unique_vars_name(vars_: Optional[CommentedMap], var_name: str):
+        while vars_ and var_name in vars_:
+            var_name += "_"
+        return var_name
 
     # noinspection PyUnusedLocal
     def _replace_with_list(
@@ -90,39 +122,71 @@ class UseLoopInsteadOfWith(AnsibleLintRule, TransformMixin):
         task: Dict[str, Any],
         target_task: CommentedMap,
         jinja_env: Environment,
+        flatten_levels: Optional[int] = 1,
     ) -> bool:
         with_items = target_task[loop_type]
         vars_ = None
         if isinstance(with_items, list):
             vars_ = target_task.get("vars", CommentedMap())
-            var_name = "items"
-            while var_name in vars_:
-                var_name += "_"
+            var_name = self._unique_vars_name(vars_, "items")
             vars_[var_name] = with_items
 
             loop_value = "{{" + var_name + "}}"
         else:
             loop_value = with_items
 
-        ast = jinja_env.parse(loop_value)
-        output_node = cast(nodes.Output, ast.body[0])
-        if len(output_node.nodes) != 1:
+        loop_value = filter_with_flatten(jinja_env, loop_value, levels=flatten_levels)
+        if loop_value is None:
             # unexpected template.
             # There shouldn't be TemplateData nodes or more than one expression.
             return False
-        node: nodes.Node = output_node.nodes[0]
-        output_node.nodes[0] = nodes.Filter(
-            node, "flatten", [], [nodes.Keyword("levels", nodes.Const(1))], None, None
-        )
-        loop_value = cast(str, dump(node=ast, environment=jinja_env))
 
-        if vars_ is not None:
-            comment = target_task.ca.items.pop("vars", None)
-            target_task["vars"] = vars_
-            if comment is not None:
-                target_task.ca.items["vars"] = comment
-
+        self._set_vars(vars_, target_task)
         self._set_loop_value(loop_type, loop_value, target_task)
+        return True
+
+    # flattend lookup is in the community.general collection (see note above)
+    _replace_with_flattened = functools.partialmethod(
+        _replace_with_items, flatten_levels=None
+    )
+
+    def _replace_with_indexed_items(
+        self,
+        loop_type: str,
+        task: Dict[str, Any],
+        target_task: CommentedMap,
+        jinja_env: Environment,
+    ) -> bool:
+        with_indexed_items = target_task[loop_type]
+        vars_ = target_task.get("vars", None)
+        if isinstance(with_indexed_items, list):
+            if vars_ is None:
+                vars_ = CommentedMap()
+            var_name = self._unique_vars_name(vars_, "items")
+            vars_[var_name] = with_indexed_items
+
+            loop_value = "{{" + var_name + "}}"
+        else:
+            loop_value = with_indexed_items
+
+        index_var_name = self._unique_vars_name(vars_, "index")
+
+        loop_value = filter_with_flatten(jinja_env, loop_value, levels=1)
+        if loop_value is None:
+            # unexpected template.
+            # There shouldn't be TemplateData nodes or more than one expression.
+            return False
+
+        position = list(target_task.keys()).index(loop_type)
+
+        self._set_vars(vars_, target_task)
+        self._set_loop_value(loop_type, loop_value, target_task)
+
+        if "loop_control" not in target_task:
+            target_task.insert(position + 1, "loop_control", CommentedMap())
+        elif not isinstance(target_task["loop_control"], MutableMapping):
+            target_task["loop_control"] = CommentedMap()
+        target_task["loop_control"]["index_var"] = index_var_name
         return True
 
 
