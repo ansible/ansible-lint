6169349c47d8d928cc06d98b1573c2d4e6f6f4b2
Jacob Floyd <cognifloyd@gmail.com>
2022-02-10 15:15:19 -0600

    Satisfy pylint. Will reintroduce matches var in subsequent PR.

--- src/ansiblelint/transformer.py
+++ src/ansiblelint/transformer.py
@@ -49,7 +49,7 @@ class Transformer:
     def run(self) -> None:
         """For each file, read it, execute transforms on it, then write it."""
         yaml = FormattedYAML()
-        for file, matches in self.matches_per_file.items():
+        for file, _ in self.matches_per_file.items():
             # str() convinces mypy that "text/yaml" is a valid Literal.
             # Otherwise, it thinks base_kind is one of playbook, meta, tasks, ...
             file_is_yaml = str(file.base_kind) == "text/yaml"
