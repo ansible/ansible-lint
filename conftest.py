253d90f23a1675528463292ceff8a2fdd73b72f1
Sorin Sbarnea <ssbarnea@redhat.com>
2022-02-17 12:08:17 +0000

    Improve contributor experience
    - ensure venv folder is ignored
    - add linters used by vscode to test extra so the IDE does not
      prompt about installing them
    - check for missing modules in conftest.py in order to help user
      install all the test dependencies

--- conftest.py
+++ conftest.py
@@ -1,5 +1,20 @@
 """PyTest Fixtures."""
+import importlib
 import os
+import sys
+
+# checking if user is running pytest without installing test dependencies:
+missing = []
+for module in ["ansible", "black", "flake8", "flaky", "mypy", "pylint", "pytest_cov"]:
+    if not importlib.util.find_spec(module):
+        missing.append(module)
+if missing:
+    print(
+        f"FATAL: Missing modules: {', '.join(missing)} -- probably you missed installing test requirements with: pip install -e '.[test]'",
+        file=sys.stderr,
+    )
+    sys.exit(1)
+
 
 os.environ["NO_COLOR"] = "1"
 pytest_plugins = ["ansiblelint.testing.fixtures"]
