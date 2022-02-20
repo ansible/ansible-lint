e5f609916c50e2c60a5a455fac82544f59e850bb
Jacob Floyd <cognifloyd@gmail.com>
2022-02-20 00:57:56 -0600

    Drop unnecessary Transformer changes

--- src/ansiblelint/transformer.py
+++ src/ansiblelint/transformer.py
@@ -1,15 +1,10 @@
 """Transformer implementation."""
 import logging
-from typing import List, Optional, Set
-
-# Module 'ruamel.yaml' does not explicitly export attribute 'YAML'; implicit reexport disabled
-# To make the type checkers happy, we import from ruamel.yaml.main instead.
-from ruamel.yaml.main import YAML
+from typing import List, Set
 
 from ansiblelint.errors import MatchError
 from ansiblelint.file_utils import Lintable
 from ansiblelint.runner import LintResult
-from ansiblelint.yaml_utils import yaml_round_tripper
 
 __all__ = ["Transformer"]
 
@@ -26,17 +21,8 @@ class Transformer:
 
     def __init__(self, result: LintResult):
         """Initialize a Transformer instance."""
-        self._yaml: Optional[YAML] = None
-
         self.matches: List[MatchError] = result.matches
         self.files: Set[Lintable] = result.files
-
-    @property
-    def yaml(self) -> YAML:
-        if self._yaml is None:
-            # TODO: Use options to configure yaml_round_tripper here.
-            self._yaml = yaml_round_tripper()
-        return self._yaml
 
     def run(self) -> None:
         """For each file, read it, execute transforms on it, then write it."""
