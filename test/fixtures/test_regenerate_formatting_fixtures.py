317f2578966b81f63ed9391a7dcc68ad431d24aa
Sorin Sbarnea <ssbarnea@redhat.com>
2022-02-25 10:27:41 +0000

    Avoid using implementation detail in formatting fixtures

--- test/fixtures/test_regenerate_formatting_fixtures.py
+++ test/fixtures/test_regenerate_formatting_fixtures.py
@@ -21,19 +21,19 @@ def test_regenerate_formatting_fixtures() -> None:
     fixtures_dir = Path("test/fixtures/")
     fixtures_dir_before = fixtures_dir / "formatting-before"
     fixtures_dir_prettier = fixtures_dir / "formatting-prettier"
-    fixtures_dir_ruamel_yaml = fixtures_dir / "formatting-ruamel-yaml"
+    fixtures_dir_after = fixtures_dir / "formatting-after"
 
     fixtures_dir_prettier.mkdir(exist_ok=True)
-    fixtures_dir_ruamel_yaml.mkdir(exist_ok=True)
+    fixtures_dir_after.mkdir(exist_ok=True)
 
     print("\nCopying before fixtures...")
     for fixture in fixtures_dir_before.glob("fmt-[0-9].yml"):
         shutil.copy(str(fixture), str(fixtures_dir_prettier / fixture.name))
-        shutil.copy(str(fixture), str(fixtures_dir_ruamel_yaml / fixture.name))
+        shutil.copy(str(fixture), str(fixtures_dir_after / fixture.name))
 
     print("\nWriting fixtures with prettier...")
     subprocess.check_call(["prettier", "-w", str(fixtures_dir_prettier)])
     # NB: pre-commit end-of-file-fixer can also modify files.
 
     # prepare ruamel.yaml fixtures (diff in next PR will show how it compares).
-    subprocess.check_call(["prettier", "-w", str(fixtures_dir_ruamel_yaml)])
+    subprocess.check_call(["prettier", "-w", str(fixtures_dir_after)])
