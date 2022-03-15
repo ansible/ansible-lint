dc16c9639260c1b3ae6e72b60d2783e85e676eae
Sorin Sbarnea <ssbarnea@redhat.com>
2022-03-15 13:29:13 +0000

    Update eco job output

--- test/test_eco.py
+++ test/test_eco.py
@@ -1,9 +1,12 @@
 """Test a set of 3rd party Ansible repositories for possible regressions."""
 import os
 import pathlib
+import shlex
 import subprocess
 
 import pytest
+
+from ansiblelint.testing import run_ansible_lint
 
 eco_repos = {
     "bootstrap": [
@@ -47,12 +50,28 @@ def test_eco(repo: str) -> None:
         subprocess.run(f"git clone {url} {cache_dir}/{repo}", shell=True, check=True)
     # run ansible lint and paths from user home in order to produce
     # consistent results regardless on its location.
-    subprocess.run(
-        # we exclude `fqcn-builtins` until repository owners fix it.
-        f'ansible-lint -f pep8 -x fqcn-builtins 2>&1 | sed "s:${{HOME}}:~:g" > {my_dir}/{repo}.result',
-        shell=True,
-        check=False,
+
+    # we exclude `fqcn-builtins` until repository owners fix it.
+    args = ["-f", "pep8", "-x", "fqcn-builtins"]
+    result = run_ansible_lint(
+        *args,
+        executable="ansible-lint",
         cwd=f"{cache_dir}/{repo}",
     )
+
+    def rel_home(text: str) -> str:
+        """Replace full path to home directory with ~."""
+        return text.replace(os.path.expanduser("~"), "~")
+
+    result_txt = f"CMD: {shlex.join(result.args)}\n\nRC: {result.returncode}\n\nSTDERR:\n{result.stderr}\n\nSTDOUT:\n{result.stdout}"
+
+    with open(f"{my_dir}/{repo}.result", "w", encoding="utf-8") as f:
+        f.write(rel_home(result_txt))
     # fail if result is different than our expected one:
-    subprocess.run(f"git diff HEAD test/eco/{repo}.result", shell=True, check=True)
+    result = subprocess.run(
+        f"git diff --exit-code test/eco/{repo}.result",
+        shell=True,
+        check=False,
+        capture_output=True,
+    )
+    assert result.returncode == 0, result_txt
