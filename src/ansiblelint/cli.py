ansible/ansible-lint

commit 64d29aba925b31372be8d89cf2761ee873d9f526

Avoid looking for config file outside current git repo (#1436)
This fixes a bug where running the linter inside a project that did
not had a config file could end-up picking a config file from another
project (was happening only when projects were nested).

From now on, we stop looking at parent directories when we encounter
the .git folder, which can be seen as project boundary.

src/ansiblelint/cli.py
@@ -109,6 +109,11 @@ def get_config_path(config_file: str = '.ansible-lint') -> Optional[str]:
             filename = os.path.abspath(os.path.join(parent, project_filename))
             if os.path.exists(filename):
                 return filename
+            if os.path.exists(os.path.abspath(os.path.join(parent, '.git'))):
+                # Avoid looking outside .git folders as we do not want endup
+                # picking config files from upper level projects if current
+                # project has no config.
+                return None
         (parent, tail) = os.path.split(parent)
     return None
 

commit 5a639b078b15448c4d2dca4e9f9aaedef83199fc

fix loading ansible-lint.yml in git projects
try both config names before checking .git

The bug was introduced in 64d29aba925b31372be8d89cf2761ee873d9f526, but
didn't affect anything until 1013149988e0959312c3a6f7fb22f2fe98ba33ad
added a second possible config file name

Fixes: #2045

src/ansiblelint/cli.py
@@ -101,11 +101,11 @@ def get_config_path(config_file: Optional[str] = None) -> Optional[str]:
             filename = os.path.abspath(os.path.join(parent, project_filename))
             if os.path.exists(filename):
                 return filename
-            if os.path.exists(os.path.abspath(os.path.join(parent, ".git"))):
-                # Avoid looking outside .git folders as we do not want end-up
-                # picking config files from upper level projects if current
-                # project has no config.
-                return None
+        if os.path.exists(os.path.abspath(os.path.join(parent, ".git"))):
+            # Avoid looking outside .git folders as we do not want end-up
+            # picking config files from upper level projects if current
+            # project has no config.
+            return None
         (parent, tail) = os.path.split(parent)
     return None
 