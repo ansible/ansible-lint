46d54945a45948803104ff72c5260dc3b453e6d6
notok <noto.kazufumi@gmail.com>
2022-04-02 18:04:15 +0900

    Apply var-spacing tests to vars files

--- src/ansiblelint/rules/var_spacing.py
+++ src/ansiblelint/rules/var_spacing.py
@@ -4,11 +4,18 @@
 
 import re
 import sys
-from typing import Any, Dict, List, Optional, Union
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
+
+from ansible.parsing.yaml.objects import AnsibleUnicode
 
 from ansiblelint.file_utils import Lintable
 from ansiblelint.rules import AnsibleLintRule
+from ansiblelint.skip_utils import get_rule_skips_from_line
+from ansiblelint.utils import parse_yaml_from_file
 from ansiblelint.yaml_utils import nested_items_path
+
+if TYPE_CHECKING:
+    from ansiblelint.errors import MatchError
 
 
 class VariableHasSpacesRule(AnsibleLintRule):
@@ -33,8 +40,42 @@ class VariableHasSpacesRule(AnsibleLintRule):
                     return self.shortdesc.format(var_name=v)
         return False
 
+    def matchyaml(self, file: Lintable) -> List["MatchError"]:
+        """Return matches for variables defined in vars files."""
+        data: Dict[str, Any] = {}
+        raw_results: List["MatchError"] = []
+        results: List["MatchError"] = []
 
-if "pytest" in sys.modules:
+        if str(file.kind) == "vars":
+            data = parse_yaml_from_file(str(file.path))
+            for k, v, path in nested_items_path(data):
+                if isinstance(v, AnsibleUnicode):
+                    cleaned = self.exclude_json_re.sub("", v)
+                    if bool(self.bracket_regex.search(cleaned)):
+                        path_elem = [
+                            f"[{i}]" if isinstance(i, int) else i for i in path + [k]
+                        ]
+                        raw_results.append(
+                            self.create_matcherror(
+                                filename=file,
+                                linenumber=v.ansible_pos[1],
+                                message=self.shortdesc.format(var_name=v),
+                                details=f".{'.'.join(path_elem)}",
+                            )
+                        )
+            if raw_results:
+                lines = file.content.splitlines()
+                for match in raw_results:
+                    # linenumber starts with 1, not zero
+                    skip_list = get_rule_skips_from_line(lines[match.linenumber - 1])
+                    if match.rule.id not in skip_list and match.tag not in skip_list:
+                        results.append(match)
+        else:
+            results.extend(super().matchyaml(file))
+        return results
+
+
+if "pytest" in sys.modules:  # noqa: C901
 
     import pytest
 
@@ -67,5 +108,58 @@ if "pytest" in sys.modules:
         # list unexpected error lines or non-matching error lines
         error_lines_difference = list(
             set(error_expected_lines).symmetric_difference(set(lint_error_lines))
+        )
+        assert len(error_lines_difference) == 0
+
+    # Test for vars file
+    @pytest.fixture(name="error_expected_details_varsfile")
+    def fixture_error_expected_details_varsfile() -> List[str]:
+        """Return list of expected error details."""
+        return [
+            ".bad_var_1",
+            ".bad_var_2",
+            ".bad_var_3",
+            ".invalid_multiline_nested_json",
+            ".invalid_nested_json",
+        ]
+
+    @pytest.fixture(name="error_expected_lines_varsfile")
+    def fixture_error_expected_lines_varsfile() -> List[int]:
+        """Return list of expected error lines."""
+        return [12, 13, 14, 27, 33]
+
+    @pytest.fixture(name="test_varsfile_path")
+    def fixture_test_varsfile_path() -> str:
+        """Return test cases vars file path."""
+        return "examples/playbooks/vars/var-spacing.yml"
+
+    @pytest.fixture(name="lint_error_results_varsfile")
+    def fixture_lint_error_results_varsfile(
+        test_varsfile_path: str,
+    ) -> List["MatchError"]:
+        """Get VarHasSpacesRules linting results on test_vars."""
+        collection = RulesCollection()
+        collection.register(VariableHasSpacesRule())
+        lintable = Lintable(test_varsfile_path)
+        results = Runner(lintable, rules=collection).run()
+        return results
+
+    def test_var_spacing_vars(
+        error_expected_details_varsfile: List[str],
+        error_expected_lines_varsfile: List[int],
+        lint_error_results_varsfile: List["MatchError"],
+    ) -> None:
+        """Ensure that expected error details are matching found linting error details."""
+        details = list(map(lambda item: item.details, lint_error_results_varsfile))
+        # list unexpected error details or non-matching error details
+        error_details_difference = list(
+            set(error_expected_details_varsfile).symmetric_difference(set(details))
+        )
+        assert len(error_details_difference) == 0
+
+        lines = list(map(lambda item: item.linenumber, lint_error_results_varsfile))
+        # list unexpected error lines or non-matching error lines
+        error_lines_difference = list(
+            set(error_expected_lines_varsfile).symmetric_difference(set(lines))
         )
         assert len(error_lines_difference) == 0
