f3759417c7db890b3d37ebbed208f205bf54816e
Nick Miller <nick.miller54@gmail.com>
2022-05-04 22:34:20 -0400

    Add check for making sure name if the first attribute in task

---
+++ src/ansiblelint/rules/task_name_first.py
@@ -0,0 +1,87 @@
+"""All tasks should be have name be the first attribute"""
+import sys
+from typing import TYPE_CHECKING, Any, Dict, Union, List
+
+from ansiblelint.rules import AnsibleLintRule
+from ansiblelint.file_utils import Lintable
+
+from typing import Optional
+
+class TaskHasNameFirstRule(AnsibleLintRule):
+    id = 'task-name-first'
+    shortdesc = __doc__
+    severity = 'LOW'
+    tags = ['opt-in', 'formatting', 'experimental']
+    version_added = 'v6.0.2'
+    needs_raw_task = True
+
+    def matchtask(
+        self, task: Dict[str, Any], file: Optional[Lintable] = None
+    ) -> Union[bool, str]:
+        raw_task = task['__raw_task__']
+        attribute_list = [*raw_task]
+
+        return attribute_list[0] != 'name'
+
+
+# testing code to be loaded only with pytest or when executed the rule file
+if "pytest" in sys.modules:
+
+    import pytest
+    from ansiblelint.testing import RunFromText
+
+    PLAY_FAIL = """---
+- hosts: localhost
+  tasks:
+    - no_log: true
+      shell: echo hello
+      name: task with no_log on top
+    - when: true
+      name: task with when on top
+      shell: echo hello
+    - delegate_to: localhost
+      name: delegate_to on top
+      shell: echo hello
+    - loop:
+        - 1
+        - 2
+      name: loopy
+      command: echo {{ item }}
+    - become: true
+      name: become first
+      shell: echo hello
+    - register: test
+      shell: echo hello
+      name: register first
+"""
+
+    PLAY_SUCCESS = """---
+- hosts: localhost
+  tasks:
+    - name: test
+      command: echo "test"
+    - name: test2
+      debug:
+        msg: "Debug without a name"
+    - name: Flush handlers
+      meta: flush_handlers
+    - no_log: true  # noqa task-name-first
+      shell: echo hello
+      name: task with no_log on top
+"""
+
+    @pytest.mark.parametrize(
+        'rule_runner', (TaskHasNameFirstRule,), indirect=['rule_runner']
+    )
+    def test_task_name_has_name_first_rule_pass(rule_runner: RunFromText) -> None:
+        """Test rule matches."""
+        results = rule_runner.run_playbook(PLAY_SUCCESS)
+        assert len(results) == 0
+
+    @pytest.mark.parametrize(
+        'rule_runner', (TaskHasNameFirstRule,), indirect=['rule_runner']
+    )
+    def test_task_name_has_name_first_rule_fail(rule_runner: RunFromText) -> None:
+        """Test rule matches."""
+        results = rule_runner.run_playbook(PLAY_FAIL)
+        assert len(results) == 6
