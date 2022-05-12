ad4cc87940449cd4ddc8a742c986c0b5c73b3f0e
Nick Miller <nick.miller@philips.com>
2022-05-12 02:57:06 -0400

    Add rule to check key order (#2108)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Sorin Sbarnea <sorin.sbarnea@gmail.com>
    Co-authored-by: Sorin Sbarnea <ssbarnea@redhat.com>

--- test/test_rules_collection.py
+++ test/test_rules_collection.py
@@ -159,4 +159,4 @@ def test_rules_id_format() -> None:
         assert rule_id_re.match(
             rule.id
         ), f"Rule id {rule.id} did not match our required format."
-    assert len(rules) == 41
+    assert len(rules) == 42
