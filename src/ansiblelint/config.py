b12740ff88f28152b56d8ff7882639d359bf8108
Sorin Sbarnea <ssbarnea@redhat.com>
2022-05-12 18:57:21 +0100

    schema: add ability to recognize ansible-lint config files

--- src/ansiblelint/config.py
+++ src/ansiblelint/config.py
@@ -12,6 +12,8 @@ DEFAULT_KINDS = [
     {"yaml": ".github/**/*.{yaml,yml}"},  # github workflows
     {"text": "**/templates/**/*.*"},  # templates are likely not validable
     {"execution-environment": "**/execution-environment.yml"},
+    {"ansible-lint-config": "**/.ansible-lint"},
+    {"ansible-lint-config": "**/.config/ansible-lint.yml"},
     {"inventory": "**/inventory/**.yml"},
     {"requirements": "**/meta/requirements.yml"},  # v1 only
     # https://docs.ansible.com/ansible/latest/dev_guide/collections_galaxy_meta.html
