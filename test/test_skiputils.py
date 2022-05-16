3f7e97c0f66e40432e46fe59f23e58e74da2e9c5
nishipy <41185206+nishipy@users.noreply.github.com>
2022-05-16 23:35:05 +0900

    Update to append skipped rules for nested task (#2113)
    * Update to extract tasks from nested blocks recursively.
    
    * Add is_nested_task fix error on test_import
    
    * Update get_nested_tasks()
    
    * Add tests for ansiblelint.skiputils
    
    Co-authored-by: Sorin Sbarnea <ssbarnea@redhat.com>

--- test/test_skiputils.py
+++ test/test_skiputils.py
@@ -1,8 +1,19 @@
 """Validate ansiblelint.skip_utils."""
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Dict
+
 import pytest
 
-from ansiblelint.skip_utils import get_rule_skips_from_line
+from ansiblelint.file_utils import Lintable
+from ansiblelint.skip_utils import (
+    append_skipped_rules,
+    get_rule_skips_from_line,
+    is_nested_task,
+)
 from ansiblelint.testing import RunFromText
+
+if TYPE_CHECKING:
+    from ansible.parsing.yaml.objects import AnsibleBaseYAMLObject
 
 PLAYBOOK_WITH_NOQA = """\
 ---
@@ -36,3 +47,173 @@ def test_playbook_noqa(default_text_runner: RunFromText) -> None:
     results = default_text_runner.run_playbook(PLAYBOOK_WITH_NOQA)
     # Should raise error at "SOME_VAR".
     assert len(results) == 1
+
+
+@pytest.mark.parametrize(
+    ("lintable", "yaml", "expected_form"),
+    (
+        pytest.param(
+            Lintable("examples/playbooks/noqa.yml", kind="playbook"),
+            [
+                {
+                    "hosts": "localhost",
+                    "tasks": [
+                        {
+                            "name": "this would typically fire git-latest and partial-become",
+                            "become_user": "alice",
+                            "git": "src=/path/to/git/repo dest=checkout",
+                            "__line__": 4,
+                            "__file__": Path("examples/playbooks/noqa.yml"),
+                        }
+                    ],
+                    "__line__": 2,
+                    "__file__": Path("examples/playbooks/noqa.yml"),
+                }
+            ],
+            [
+                {
+                    "hosts": "localhost",
+                    "tasks": [
+                        {
+                            "name": "this would typically fire git-latest and partial-become",
+                            "become_user": "alice",
+                            "git": "src=/path/to/git/repo dest=checkout",
+                            "__line__": 4,
+                            "__file__": Path("examples/playbooks/noqa.yml"),
+                            "skipped_rules": ["git-latest", "partial-become"],
+                        }
+                    ],
+                    "__line__": 2,
+                    "__file__": Path("examples/playbooks/noqa.yml"),
+                }
+            ],
+        ),
+        pytest.param(
+            Lintable("examples/playbooks/noqa-nested.yml", kind="playbook"),
+            [
+                {
+                    "hosts": "localhost",
+                    "tasks": [
+                        {
+                            "name": "example of multi-level block",
+                            "block": [
+                                {
+                                    "name": "2nd level",
+                                    "block": [
+                                        {
+                                            "ansible.builtin.debug": {
+                                                "msg": "test unnamed task in block",
+                                                "__line__": 9,
+                                                "__file__": Path(
+                                                    "examples/playbooks/noqa-nested.yml"
+                                                ),
+                                            },
+                                            "__line__": 8,
+                                            "__file__": Path(
+                                                "examples/playbooks/noqa-nested.yml"
+                                            ),
+                                        }
+                                    ],
+                                    "__line__": 6,
+                                    "__file__": Path(
+                                        "examples/playbooks/noqa-nested.yml"
+                                    ),
+                                }
+                            ],
+                            "__line__": 4,
+                            "__file__": Path("examples/playbooks/noqa-nested.yml"),
+                        }
+                    ],
+                    "__line__": 2,
+                    "__file__": Path("examples/playbooks/noqa-nested.yml"),
+                }
+            ],
+            [
+                {
+                    "hosts": "localhost",
+                    "tasks": [
+                        {
+                            "name": "example of multi-level block",
+                            "block": [
+                                {
+                                    "name": "2nd level",
+                                    "block": [
+                                        {
+                                            "ansible.builtin.debug": {
+                                                "msg": "test unnamed task in block",
+                                                "__line__": 9,
+                                                "__file__": Path(
+                                                    "examples/playbooks/noqa-nested.yml"
+                                                ),
+                                            },
+                                            "__line__": 8,
+                                            "__file__": Path(
+                                                "examples/playbooks/noqa-nested.yml"
+                                            ),
+                                            "skipped_rules": ["unnamed-task"],
+                                        }
+                                    ],
+                                    "__line__": 6,
+                                    "__file__": Path(
+                                        "examples/playbooks/noqa-nested.yml"
+                                    ),
+                                    "skipped_rules": ["unnamed-task"],
+                                }
+                            ],
+                            "__line__": 4,
+                            "__file__": Path("examples/playbooks/noqa-nested.yml"),
+                            "skipped_rules": ["unnamed-task"],
+                        }
+                    ],
+                    "__line__": 2,
+                    "__file__": Path("examples/playbooks/noqa-nested.yml"),
+                }
+            ],
+        ),
+    ),
+)
+def test_append_skipped_rules(
+    lintable: Lintable,
+    yaml: "AnsibleBaseYAMLObject",
+    expected_form: "AnsibleBaseYAMLObject",
+) -> None:
+    """Check that it appends skipped_rules properly."""
+    assert append_skipped_rules(yaml, lintable) == expected_form
+
+
+@pytest.mark.parametrize(
+    ("task", "expected"),
+    (
+        pytest.param(
+            dict(
+                name="ensure apache is at the latest version",
+                yum={"name": "httpd", "state": "latest"},
+            ),
+            False,
+        ),
+        pytest.param(
+            dict(
+                name="Attempt and graceful roll back",
+                block=[
+                    {"name": "Force a failure", "ansible.builtin.command": "/bin/false"}
+                ],
+                rescue=[
+                    {
+                        "name": "Force a failure in middle of recovery!",
+                        "ansible.builtin.command": "/bin/false",
+                    }
+                ],
+                always=[
+                    {
+                        "name": "Always do this",
+                        "ansible.builtin.debug": {"msg": "This always executes"},
+                    }
+                ],
+            ),
+            True,
+        ),
+    ),
+)
+def test_is_nested_task(task: Dict[str, Any], expected: bool) -> None:
+    """Test is_nested_task() returns expected bool."""
+    assert is_nested_task(task) == expected
