fe0fa9873bac8a19a6a1e63ec35734ca52c756c2
Sorin Sbarnea <ssbarnea@redhat.com>
2022-05-16 09:59:51 +0100

    Convert documentation to markdown

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,10 @@ except pkg_resources.DistributionNotFound:
 templates_path = [".templates"]
 
 # The suffix of source filenames.
-source_suffix = ".rst"
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
 
 # The master toctree document.
 master_doc = "index"
