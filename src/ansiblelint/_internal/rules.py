c25b3807748a4b72e88af914cc6f877c7913ec09
Sorin Sbarnea <ssbarnea@redhat.com>
2022-06-08 17:47:32 +0100

    Mark load-failure violation as unskippable (#2201)

--- src/ansiblelint/_internal/rules.py
+++ src/ansiblelint/_internal/rules.py
@@ -10,6 +10,20 @@ if TYPE_CHECKING:
     from ansiblelint.file_utils import Lintable
 
 _logger = logging.getLogger(__name__)
+LOAD_FAILURE_MD = """\
+## load-failure
+
+Linter failed to process a YAML file, probably because it is either:
+
+* contains unsupported encoding (only UTF-8 is supported)
+* not an Ansible file
+* it contains some unsupported custom YAML objects (`!!` prefix)
+* it was not able to decrypt an inline `!vault` block.
+
+This violation **is not** skippable, so it cannot be added to the `warn_list`
+or the `skip_list`. If a vault decryption issue cannot be avoided, the
+offending file can be added to `exclude_paths` configuration.
+"""
 
 
 # Derived rules are likely to want to access class members, so:
@@ -130,5 +144,6 @@ class LoadingFailureRule(BaseRule):
     id = "load-failure"
     description = "Linter failed to process a YAML file, possible not an Ansible file."
     severity = "VERY_HIGH"
-    tags = ["core"]
+    tags = ["core", "unskippable"]
     version_added = "v4.3.0"
+    help = LOAD_FAILURE_MD
