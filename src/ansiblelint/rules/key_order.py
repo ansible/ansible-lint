a85b35ded80df7e576fa8dee7f8fcaca623e55b2
Nick Miller <nick.miller54@gmail.com>
2022-06-22 21:45:46 -0400

    Update key-order check to check all keys
    A user can set the key_order setting to one of the following options:
    
    * default - checks only the order of name and action
    * enhanced - checks a smaller list of possible keys
    * everything - checks a list containing every key

--- src/ansiblelint/rules/key_order.py
+++ src/ansiblelint/rules/key_order.py
@@ -1,9 +1,11 @@
-"""All tasks should be have name come first."""
+"""Ensure specific order of keys in mappings."""
 from __future__ import annotations
 
 import sys
+from collections import OrderedDict as odict
 from typing import Any
 
+from ansiblelint.config import options
 from ansiblelint.file_utils import Lintable
 from ansiblelint.rules import AnsibleLintRule
 from ansiblelint.testing import RunFromText
@@ -13,20 +15,47 @@ class KeyOrderRule(AnsibleLintRule):
     """Ensure specific order of keys in mappings."""
 
     id = "key-order"
+    description = """\
+Keys should be in the specified order. In the default configuration, it only enforces name first. Checking the order of all keys can be anabled by setting 'key_order' in the config
+"""
     shortdesc = __doc__
     severity = "LOW"
     tags = ["formatting", "experimental"]
     version_added = "v6.2.0"
     needs_raw_task = True
 
+    # skipped rules is not a key
+    removed_keys = ["skipped_rules"]
+    possible_keys = options.key_order
+    if options.custom_key_order:
+        possible_keys = options.custom_key_order
+
+    ordered_expected_keys = odict((key, idx) for idx, key in enumerate(possible_keys))
+
     def matchtask(
         self, task: dict[str, Any], file: Lintable | None = None
     ) -> bool | str:
-        raw_task = task["__raw_task__"]
-        if "name" in raw_task:
-            attribute_list = [*raw_task]
-            if bool(attribute_list[0] != "name"):
-                return "'name' key is not first"
+        keys = task["__raw_task__"].keys()
+
+        # get the expected order in from the lookup table
+        actual_order = odict()
+        for attr in keys:
+            if not attr.startswith("__") and (attr not in self.removed_keys):
+                pos = self.ordered_expected_keys.get(attr)
+                if pos is None:
+                    pos = self.ordered_expected_keys.get("action")
+                actual_order[attr] = pos
+
+        sorted_actual_order = odict(
+            sorted(
+                actual_order.items(),
+                key=lambda item: item[1],
+            )
+        )
+
+        if bool(sorted_actual_order != actual_order):
+            text = ",".join(sorted_actual_order.keys())
+            return f"Keys are not in order. Expected order '{text}'"
         return False
 
 
@@ -58,6 +87,12 @@ if "pytest" in sys.modules:
     - register: test
       shell: echo hello
       name: Register first
+    - tags: hello
+      no_log: true
+      name: Echo hello with tags
+      become: true
+      delegate_to: localhost
+      shell: Echo hello with tags
 """
 
     PLAY_SUCCESS = """---
@@ -70,6 +105,20 @@ if "pytest" in sys.modules:
         msg: "Debug without a name"
     - name: Flush handlers
       meta: flush_handlers
+    - name: task with no_log on top
+      no_log: true  # noqa key-order
+      shell: echo hello
+    - name: Echo hello with tags
+      become: true
+      delegate_to: localhost
+      no_log: true
+      shell: echo hello with tags
+      tags: hello
+    - name: Loopy
+      command: echo {{ item }}
+      loop:
+        - 1
+        - 2
     - no_log: true  # noqa key-order
       shell: echo hello
       name: Task with no_log on top
@@ -85,4 +134,4 @@ if "pytest" in sys.modules:
     def test_task_name_has_name_first_rule_fail(rule_runner: RunFromText) -> None:
         """Test rule matches."""
         results = rule_runner.run_playbook(PLAY_FAIL)
-        assert len(results) == 6
+        assert len(results) == 7
