659ab7c43edc3320403faba41e39b057599a9f71
nishipy <41185206+nishipy@users.noreply.github.com>
2022-07-19 22:46:14 +0900

    deprecated-local-action: refactor to use matchtask() (#2238)
    * Update deprecated-local-action to fix false positives
    
    Signed-off-by: nishipy <goodisonev4@gmail.com>
    
    * PYTEST_REQPASS
    
    Signed-off-by: nishipy <goodisonev4@gmail.com>
    
    * Update deprecated_local_action
    
    Signed-off-by: nishipy <goodisonev4@gmail.com>
    
    * Update utils
    
    Signed-off-by: nishipy <goodisonev4@gmail.com>
    
    * Update utils ans deprecated-local-action
    
    Signed-off-by: nishipy <goodisonev4@gmail.com>
    
    * Fix to use __raw_task__ in deprecated-local-action
    
    Signed-off-by: nishipy <goodisonev4@gmail.com>

--- src/ansiblelint/rules/deprecated_local_action.py
+++ src/ansiblelint/rules/deprecated_local_action.py
@@ -3,8 +3,14 @@
 # Copyright (c) 2018, Ansible Project
 
 import sys
+from typing import TYPE_CHECKING, Any, Dict, Union
 
 from ansiblelint.rules import AnsibleLintRule
+
+if TYPE_CHECKING:
+    from typing import Optional
+
+    from ansiblelint.file_utils import Lintable
 
 
 class TaskNoLocalAction(AnsibleLintRule):
@@ -12,32 +18,46 @@ class TaskNoLocalAction(AnsibleLintRule):
 
     id = "deprecated-local-action"
     description = "Do not use ``local_action``, use ``delegate_to: localhost``"
+    needs_raw_task = True
     severity = "MEDIUM"
     tags = ["deprecations"]
     version_added = "v4.0.0"
 
-    def match(self, line: str) -> bool:
-        if "local_action" in line:
+    def matchtask(
+        self, task: Dict[str, Any], file: "Optional[Lintable]" = None
+    ) -> Union[bool, str]:
+        """Return matches for a task."""
+        raw_task = task["__raw_task__"]
+        if "local_action" in raw_task.keys():
             return True
+
         return False
 
 
 # testing code to be loaded only with pytest or when executed the rule file
 if "pytest" in sys.modules:
+    import pytest
 
     from ansiblelint.rules import RulesCollection  # pylint: disable=ungrouped-imports
     from ansiblelint.testing import RunFromText
 
-    TASK_LOCAL_ACTION = """
+    FAIL_TASK = """
     - name: task example
       local_action:
         module: boto3_facts
     """
 
-    def test_local_action() -> None:
+    SUCCESS_TASK = """
+    - name: task example
+      boto3_facts:
+      delegate_to: localhost # local_action
+    """
+
+    @pytest.mark.parametrize(("text", "expected"), ((SUCCESS_TASK, 0), (FAIL_TASK, 1)))
+    def test_local_action(text: str, expected: int) -> None:
         """Positive test deprecated_local_action."""
         collection = RulesCollection()
         collection.register(TaskNoLocalAction())
         runner = RunFromText(collection)
-        results = runner.run_role_tasks_main(TASK_LOCAL_ACTION)
-        assert len(results) == 1
+        results = runner.run_role_tasks_main(text)
+        assert len(results) == expected
