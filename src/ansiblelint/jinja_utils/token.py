43c20adeabf77dee932909b14adc8b19e17afcb7
Jacob Floyd <cognifloyd@gmail.com>
2022-08-14 20:12:28 -0500

    expand jinja token handling

--- src/ansiblelint/jinja_utils/token.py
+++ src/ansiblelint/jinja_utils/token.py
@@ -1,18 +1,19 @@
 from typing import Optional
 
 from dataclasses import dataclass
-from jinja2.lexer import newline_re, Token as JinjaToken
+from jinja2.lexer import newline_re, Lexer, Token as JinjaToken
 
 
-@dataclass
+@dataclass  # this is mutable and DOES change after creation
 class Token:
     """Wrapper around JinjaToken to add details like characters consumed."""
 
     index: int
 
     # pos slices a template (ie: include start, exclude end): [start_pos:end_pos]
+    # NB: this is the pos with new lines normalized to \n
     start_pos: int
-    end_pos: int  # if including whitespace, should equal next Token's start_pos
+    end_pos: int
 
     # data from Lexer.tokeniter
     lineno: int
@@ -24,59 +25,61 @@ class Token:
     # .lineno: int
     # .type: str
     # .value: Any  # declared as str, but this can be int, float, str, etc
+    #   when str, this is normalized to use configured newline_sequence
 
     # Many tokens come in a pair. This is the other token in this pair.
     # For example: matching brackets in an expression
     # or sthe start/close of vars, blocks, comments.
     pair: Optional["Token"] = None
 
+    # chomp indicator (only used for start/end pairs) aka strip_sign
+    chomp: Literal['+', '-', ''] = ""
 
-def normalize_newlines(source: str) -> str:
+
+def pre_iter_normalize_newlines(source: str, keep_trailing_newline: bool) -> str:
     """normalize newlines in template source like lexer.tokeniter does."""
     lines = newline_re.split(source)[::2]
 
-    if not lexer.keep_trailing_newline and lines[-1] == "":
+    if not keep_trailing_newline and lines[-1] == "":
         del lines[-1]
 
     return "\n".join(lines)
 
 
-def something(
+def itertoken(
+    lexer: Lexer,
     source: str,
     name: Optional[str] = None,
     filename: Optional[str] = None,
     state: Optional[str] = None,
 ):
-    normalized_source = normalize_newlines(source)
+    normalized_source = pre_iter_normalize_newlines(source, lexer.keep_trailing_newline)
     paired_tokens: List[Token] = []
-    previous_token: Optional[Token] = None
 
     for index, token_tuple in enumerate(lexer.tokeniter(source, name, filename, state)):
-        lineno, token, value_str = token_tuple
+        lineno, raw_token, value_str = token_tuple
 
         start_pos = normalized_source.index(value_str)
-        end_pos = start_pos + len(value_str)
-        if previous_token is not None and start_pos > previous_token.end_pos:
-            # space was stripped/chomped
-            pass
+        consumed = len(value_str)
+        end_pos = start_pos + consumed
 
-        jinja_token: Optional[JinjaToken] = None  # None if skipped by wrap()
-        for jinja_token in lexer.wrap(iter([token_tuple])):
-            # if we got here, then wrap did not skip the token
-            pass
-
-        token = previous_token = Token(
-            index, start_pos, end_pos, lineno, token, value_str, jinja_token
-        )
+        jinja_token: Optional[JinjaToken]
+        try:
+            jinja_token = next(lexer.wrap(iter([token_tuple])))
+        except StopIteration:
+            # wrap skipped the token
+            jinja_token = None
 
         is_pair_opener = is_pair_closer = False
+        chomp: str = ""
+
         # see if this token should have a pair
-        if token == TOKEN_OPERATOR:
+        if raw_token == TOKEN_OPERATOR:
             if value_str in ("{", "(", "["):
                 is_pair_opener = True
             elif value_str in ("}", ")", "]"):
                 is_pair_closer = True
-        elif token in (
+        elif raw_token in (
             TOKEN_BLOCK_BEGIN,
             TOKEN_VARIABLE_BEGIN,
             TOKEN_RAW_BEGIN,
@@ -85,7 +88,10 @@ def something(
             TOKEN_LINECOMMENT_BEGIN,
         ):
             is_pair_opener = True
-        elif token in (
+            if normalized_source[end_pos] in ("+", "-"):
+                chomp = normalized_source[end_pos]
+                end_pos += 1
+        elif raw_token in (
             TOKEN_BLOCK_END,
             TOKEN_VARIABLE_END,
             TOKEN_RAW_END,
@@ -94,7 +100,15 @@ def something(
             TOKEN_LINECOMMENT_END,
         ):
             is_pair_closer = True
+            if normalized_source[start_pos-1] in ("+", "-"):
+                chomp = normalized_source[start_pos-1]
+                start_pos -= 1
 
+        token = Token(
+            index, start_pos, end_pos, lineno, raw_token, value_str, jinja_token, chomp
+        )
+
+        # we assume the template is valid and was previously fully parsed
         if is_pair_opener:
             paired_tokens.append(token)
         elif is_pair_closer:
