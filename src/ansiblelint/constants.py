500aeeb0b1bdd962c7d3481783051e0bec7cee06
Sorin Sbarnea <ssbarnea@redhat.com>
2022-08-30 14:37:22 +0100

    Add a 60s timeout for lock.aquire() (#2323)

--- src/ansiblelint/constants.py
+++ src/ansiblelint/constants.py
@@ -9,6 +9,7 @@ RULE_DOC_URL = "https://ansible-lint.readthedocs.io/en/latest/rules/"
 SUCCESS_RC = 0
 VIOLATIONS_FOUND_RC = 2
 INVALID_CONFIG_RC = 3
+LOCK_TIMEOUT_RC = 4
 EXIT_CONTROL_C_RC = 130
 
 # Minimal version of Ansible we support for runtime
