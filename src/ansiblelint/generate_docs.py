ea17867dcd06efdaaf083ceab0878b511d1b1f9a
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2022-09-01 16:17:29 +0100

    Correct example of selecting profile from cli
    Fixes: #2266

--- src/ansiblelint/generate_docs.py
+++ src/ansiblelint/generate_docs.py
@@ -136,7 +136,7 @@ One of the best ways to run `ansible-lint` is by specifying which rule profile
 you want to use. These profiles stack on top of each other, allowing you to
 gradually raise the quality bar.
 
-To run it with the most strict profile just type `ansible-lint -P production`.
+To run it with the most strict profile just type `ansible-lint --profile=production`.
 
 If you want to consult the list of rules from each profile, type
 `ansible-lint -P`. For your convenience, we also list the same output below.
