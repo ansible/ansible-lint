0ea03a3da06c48f457e4524a2058e1da4868af73
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2022-09-16 21:04:19 +0100

    Make links to rule docs relative

--- docs/conf.py
+++ docs/conf.py
@@ -265,7 +265,7 @@ html_copy_source = False
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-html_use_opensearch = "https://ansible-lint.readthedocs.io/en/latest/"
+html_use_opensearch = "https://ansible-lint.readthedocs.io/"
 
 # If nonempty, this is the file name suffix for HTML files (e.g. ".xhtml").
 # html_file_suffix = ''
