7449f1e3929496654545f08ffd9986c6c015fc35
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2022-09-16 19:42:44 +0100

    Fix linting errors

--- src/ansiblelint/rules/template_instead_of_copy.py
+++ src/ansiblelint/rules/template_instead_of_copy.py
@@ -1,10 +1,12 @@
 """Implementation of template-instead-of-copy rule."""
+# cspell:disable-next-line
 # Copyright (c) 2022, Alexander Skiba
 # references
 # - github discussion of issue: https://github.com/ansible/ansible/issues/50580
 # - ansible docs with warning: https://docs.ansible.com/ansible/latest/collections/ansible/builtin/copy_module.html#synopsis
+from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Dict, Union
+from typing import TYPE_CHECKING, Any
 
 from ansiblelint.rules import AnsibleLintRule
 
@@ -15,6 +17,8 @@ if TYPE_CHECKING:
 
 
 class UseTemplateInsteadOfCopyRule(AnsibleLintRule):
+    """Rule that identifies improper use copy instead of template."""
+
     id = "template-instead-of-copy"
     shortdesc = "Templated files should use template instead of copy"
     description = (
@@ -26,8 +30,9 @@ class UseTemplateInsteadOfCopyRule(AnsibleLintRule):
     version_added = "custom"
 
     def matchtask(
-        self, task: Dict[str, Any], file: "Optional[Lintable]" = None
-    ) -> Union[bool, str]:
+        self, task: dict[str, Any], file: Lintable | None = None
+    ) -> bool | str:
+        """Confirm if current rule is matching a specific task."""
         if task["action"]["__ansible_module__"] == "copy":
             if task["action"].get("content"):
                 return True
