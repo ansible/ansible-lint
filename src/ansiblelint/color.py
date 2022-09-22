55068d7752e18ab3e0cbf35fb651d86b40948d03
Sorin Sbarnea <ssbarnea@redhat.com>
2022-09-22 20:52:39 +0100

    Change rich markdown theme/colors (#2486)
    * Change rich markdown theme/colors
    
    * chore: auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/ansiblelint/color.py
+++ src/ansiblelint/color.py
@@ -4,9 +4,52 @@ from __future__ import annotations
 from typing import Any
 
 import rich
+import rich.markdown
 from rich.console import Console
+from rich.default_styles import DEFAULT_STYLES
+from rich.style import Style
 from rich.syntax import Syntax
 from rich.theme import Theme
+
+# WARNING: When making style changes, be sure you test the output of
+# `ansible-lint -L` on multiple terminals with dark/light themes, including:
+# - iTerm2 (macOS) - bold might not be rendered differently
+# - vscode integrated terminal - bold might not be rendered differently, links will not work
+#
+# When it comes to colors being used, try to match:
+# - Ansible official documentation theme, https://docs.ansible.com/ansible/latest/dev_guide/developing_api.html
+# - VSCode Ansible extension for syntax highlighting
+# - GitHub markdown theme
+#
+# Current values: (docs)
+# codeblock border: #404040
+# codeblock background: #edf0f2
+# codeblock comment: #6a737d (also italic)
+# teletype-text: #e74c3c (red)
+# teletype-text-border: 1px solid #e1e4e5 (background white)
+# text: #404040
+# codeblock other-text: #555555 (black-ish)
+# codeblock property: #22863a (green)
+# codeblock integer: 032f62 (blue)
+# codeblock command: #0086b3 (blue) - [shell]
+# == python ==
+# class: #445588 (dark blue and bold)
+# docstring: #dd1144 (red)
+# self: #999999 (light-gray)
+# method/function: #990000 (dark-red)
+# number: #009999 cyan
+# keywords (def,None,False,len,from,import): #007020 (green) bold
+# super|dict|print: #0086b3 light-blue
+# __name__: #bb60d5 (magenta)
+# string: #dd1144 (light-red)
+DEFAULT_STYLES.update(
+    {
+        # "code": Style(color="bright_black", bgcolor="red"),
+        "markdown.code": Style(color="bright_black"),
+        "markdown.code_block": Style(dim=True, color="cyan"),
+    }
+)
+
 
 _theme = Theme(
     {
@@ -44,3 +87,36 @@ def reconfigure(new_options: dict[str, Any]) -> None:
 def render_yaml(text: str) -> Syntax:
     """Colorize YAMl for nice display."""
     return Syntax(text, "yaml", theme="ansi_dark")
+
+
+# pylint: disable=redefined-outer-name,unused-argument
+def _rich_heading_custom_rich_console(
+    self: rich.markdown.Heading,
+    console: rich.console.Console,
+    options: rich.console.ConsoleOptions,
+) -> rich.console.RenderResult:
+    """Override for rich console heading."""
+    yield f"[bold]{self.level * '#'} {self.text}[/]"
+
+
+# pylint: disable=redefined-outer-name,unused-argument
+def _rich_codeblock_custom_rich_console(
+    self: rich.markdown.CodeBlock,
+    console: Console,
+    options: rich.console.ConsoleOptions,
+) -> rich.console.RenderResult:
+    code = str(self.text).rstrip()
+    syntax = Syntax(
+        code,
+        self.lexer_name,
+        theme=self.theme,
+        word_wrap=True,
+        background_color="default",
+    )
+    yield syntax
+
+
+# Monkey-patch rich to alter its rendering of headings
+# https://github.com/python/mypy/issues/2427
+rich.markdown.Heading.__rich_console__ = _rich_heading_custom_rich_console  # type: ignore
+rich.markdown.CodeBlock.__rich_console__ = _rich_codeblock_custom_rich_console  # type: ignore
