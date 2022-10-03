391cb88e09e17891b1196c3051a974fb4812a385
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2022-10-03 22:41:31 +0100

    Rename no-shorthand to no-free-form
    Reasoning: be in sync with current ansible documentation.

--- src/ansiblelint/rules/no_free_form.py
+++ src/ansiblelint/rules/no_free_form.py
@@ -1,4 +1,4 @@
-"""Implementation of NoShorthandRule."""
+"""Implementation of NoFreeFormRune."""
 from __future__ import annotations
 
 import sys
@@ -13,11 +13,11 @@ if TYPE_CHECKING:
     from ansiblelint.file_utils import Lintable
 
 
-class NoShorthandRule(AnsibleLintRule):
-    """Rule for detecting discouraged shorthand syntax for action modules."""
+class NoFreeFormRune(AnsibleLintRule):
+    """Rule for detecting discouraged free-form syntax for action modules."""
 
-    id = "no-shorthand"
-    description = "Avoid shorthand inside files as it can produce subtile bugs."
+    id = "no-free-form"
+    description = "Avoid free-form inside files as it can produce subtile bugs."
     severity = "MEDIUM"
     tags = ["syntax", "risk", "experimental"]
     version_added = "v6.8.0"
@@ -57,7 +57,7 @@ class NoShorthandRule(AnsibleLintRule):
         elif isinstance(action_value, str) and "=" in action_value:
             results.append(
                 self.create_matcherror(
-                    message=f"Avoid using shorthand (free-form) when calling module actions. ({action})",
+                    message=f"Avoid using free-form when calling module actions. ({action})",
                     linenumber=task[LINE_NUMBER_KEY],
                     filename=file,
                 )
@@ -75,16 +75,16 @@ if "pytest" in sys.modules:  # noqa: C901
     @pytest.mark.parametrize(
         ("file", "expected"),
         (
-            pytest.param("examples/playbooks/rule-no-shorthand-pass.yml", 0, id="pass"),
-            pytest.param("examples/playbooks/rule-no-shorthand-fail.yml", 2, id="fail"),
+            pytest.param("examples/playbooks/rule-no-free-form-pass.yml", 0, id="pass"),
+            pytest.param("examples/playbooks/rule-no-free-form-fail.yml", 2, id="fail"),
         ),
     )
-    def test_rule_no_shorthand(
+    def test_rule_no_free_form(
         default_rules_collection: RulesCollection, file: str, expected: int
     ) -> None:
         """Validate that rule works as intended."""
         results = Runner(file, rules=default_rules_collection).run()
 
         for result in results:
-            assert result.rule.id == NoShorthandRule.id, result
+            assert result.rule.id == NoFreeFormRune.id, result
         assert len(results) == expected
