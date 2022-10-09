c1a474caae948da648672a239e87ae517cce85b0
Felix Fontein <felix@fontein.de>
2022-10-09 13:49:05 +0200

    Add internal_names rule.

---
+++ src/ansiblelint/rules/internal_names.md
@@ -0,0 +1,35 @@
+# internal_names
+
+This rule checks for internal names being used in Ansible content.
+
+Some collections provide content under multiple names. For example, community.general and community.network allow to
+reference actions under a long FQCN and a short FQCN. The long FQCN is an implementation detail and must not be used,
+as it can change at any time (even in bugfix releases) and using it can render your playbooks and roles nonfunctional.
+
+The `internal_names` rule has the following checks:
+
+- `internal_names[community.general]` - Checks for internal names from the `community.general` collection.
+- `internal_names[community.network]` - Checks for internal names from the `community.network` collection.
+
+## Problematic Code
+
+```yaml
+---
+- name: Example playbook
+  hosts: all
+  tasks:
+    - name: Create an SSH connection
+      community.general.system.ufw:  # <- Does use an internal FQCN for the ufw module
+        ...
+```
+
+## Correct Code
+
+```yaml
+- name: Example playbook
+  hosts: all
+  tasks:
+    - name: Create an SSH connection
+      community.general.ufw:
+        ...
+```
---
+++ src/ansiblelint/rules/internal_names.py
@@ -0,0 +1,105 @@
+"""Rule definition for avoiding internal names for certain collections."""
+from __future__ import annotations
+
+import sys
+from typing import Any
+
+from ansiblelint.errors import MatchError
+from ansiblelint.file_utils import Lintable
+from ansiblelint.rules import AnsibleLintRule
+
+
+COLLECTIONS_WITH_FLATMAPPING = [
+    "community.general",
+    "community.network",
+]
+
+
+class InternalNamesRule(AnsibleLintRule):
+    """Avoid internal names for actions."""
+
+    id = "internal_names"
+    severity = "VERY_HIGH"
+    description = "Check whether actions are using internal names from collections names that should not be used."
+    tags = ["formatting"]
+    version_added = "v6.9.0"
+
+    def matchtask(
+        self, task: dict[str, Any], file: Lintable | None = None
+    ) -> list[MatchError]:
+        result = []
+        module = task["action"]["__ansible_module_original__"]
+        parts = module.split(".")
+        if len(parts) <= 3:
+            return result
+        collection = ".".join(parts[:2])
+        if collection in COLLECTIONS_WITH_FLATMAPPING:
+            if len(parts) > 3:
+                result.append(
+                    self.create_matcherror(
+                        message=f"Do not use internal name for {collection} module actions ({module}).",
+                        details=f"Use `{collection}.{parts[-1]}` instead.",
+                        filename=file,
+                        linenumber=task["__line__"],
+                        tag=f"internal_names[{collection}]",
+                    )
+                )
+        return result
+
+
+# testing code to be loaded only with pytest or when executed the rule file
+if "pytest" in sys.modules:
+
+    import pytest
+
+    from ansiblelint.testing import RunFromText  # pylint: disable=ungrouped-imports
+
+    SUCCESS_PLAY = """
+- hosts: localhost
+  tasks:
+  - name: Correct name for community.general
+    community.general.sudoers:
+      name: should-not-be-here
+      state: absent
+  - name: Correct name for community.network
+    community.network.network.edgeos.edgeos_facts:
+      gather_subset: all
+    """
+
+    FAIL_PLAY = """
+- hosts: localhost
+  tasks:
+  - name: Internal name for community.general (internal_names[community.general])
+    community.general.system.sudoers:
+      name: should-not-be-here
+      state: absent
+  - name: Internal name for community.network (internal_names[community.network])
+    community.network.edgeos_facts:
+      gather_subset: all
+    """
+
+    @pytest.mark.parametrize(
+        "rule_runner", (InternalNamesRule,), indirect=["rule_runner"]
+    )
+    def test_internal_names_fail(rule_runner: RunFromText) -> None:
+        """Test rule matches."""
+        results = rule_runner.run_playbook(FAIL_PLAY)
+        assert len(results) == 2
+        assert results[0].tag == "internal_names[community.general]"
+        assert (
+            "Do not use internal names for community.network module actions"
+            in results[0].message
+        )
+        assert results[1].tag == "internal_names[community.network]"
+        assert (
+            "Do not use internal names for community.general module actions"
+            in results[1].message
+        )
+
+    @pytest.mark.parametrize(
+        "rule_runner", (InternalNamesRule,), indirect=["rule_runner"]
+    )
+    def test_internal_names_pass(rule_runner: RunFromText) -> None:
+        """Test rule does not match."""
+        results = rule_runner.run_playbook(SUCCESS_PLAY)
+        assert len(results) == 0, results
