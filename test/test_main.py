3c52fefec83f85b3d7124134ae28ab5a6f84ac07
Arnaud Patard <apatard@hupstream.com>
2022-10-28 19:04:24 +0200

    Test get_version_warning
    This change is testing that get_version_warning():
    - returns "" if current version is latest one
    - returns a pre-release information if using pre-release
    - returns a new release information and extra installation information.
      (note: this test doesn't check that guess_install_method() is working correctly)
    
    I had to use pytest-mock to ensure that all test parameters are
    correctly set. The test should not rely on current version (since it's likely
    v1.2.3dev123) and on version on github.
    
    Signed-off-by: Arnaud Patard <apatard@hupstream.com>

--- test/test_main.py
+++ test/test_main.py
@@ -2,9 +2,13 @@
 import os
 import subprocess
 import sys
+import time
 from pathlib import Path
+from typing import Any
 
 import pytest
+
+from ansiblelint.config import get_version_warning
 
 
 @pytest.mark.parametrize(
@@ -30,3 +34,34 @@ def test_call_from_outside_venv(expected_warning: bool) -> None:
     )
     warning_found = "PATH altered to include" in proc.stderr
     assert warning_found is expected_warning
+
+
+@pytest.mark.parametrize(
+    ("ver_diff", "found", "check", "outlen"),
+    (
+        ("v1.2.2", True, "pre-release", 1),
+        ("v1.2.3", False, "", 1),
+        ("v1.2.4", True, "new release", 2),
+    ),
+)
+def test_get_version_warning(
+    mocker: Any, ver_diff: str, found: bool, check: str, outlen: int
+) -> None:
+    """Assert get_version_warning working as expected."""
+    data = '{"html_url": "https://127.0.0.1", "tag_name": "' + f"{ver_diff}" + '"}'
+    # simulate cache file
+    mocker.patch("os.path.exists", return_value=True)
+    mocker.patch("os.path.getmtime", return_value=time.time())
+    mocker.patch("builtins.open", mocker.mock_open(read_data=data))
+    # overwrite ansible-lint version
+    mocker.patch("ansiblelint.config.__version__", "1.2.3")
+    # overwrite install method to custom one. This one will increase msg line count
+    # to easily detect unwanted call to it.
+    mocker.patch("ansiblelint.config.guess_install_method", return_value="\n")
+    msg = get_version_warning()
+
+    if not found:
+        assert msg == check
+    else:
+        assert check in msg
+    assert len(msg.split("\n")) == outlen
