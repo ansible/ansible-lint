ansible/ansible-lint

commit 6e31f19ca8c0322140222afdce66cdb57ad32699

Fix offline mode (#2115)

src/ansiblelint/rules/only_builtins.py
@@ -30,9 +30,11 @@ class OnlyBuiltinsRule(AnsibleLintRule):
 # testing code to be loaded only with pytest or when executed the rule file
 if "pytest" in sys.modules:
 
+    # pylint: disable=ungrouped-imports
     import pytest
 
-    from ansiblelint.testing import RunFromText  # pylint: disable=ungrouped-imports
+    from ansiblelint.constants import VIOLATIONS_FOUND_RC
+    from ansiblelint.testing import RunFromText, run_ansible_lint
 
     SUCCESS_PLAY = """
 - hosts: localhost
@@ -41,24 +43,18 @@ if "pytest" in sys.modules:
     ansible.builtin.shell: echo This rule should not get matched by the only-builtins rule
     """
 
-    FAIL_PLAY = """
-- hosts: localhost
-  tasks:
-  - name: sysctl
-    ansible.posix.sysctl:
-      name: vm.swappiness
-      value: '5'
-    """
-
-    @pytest.mark.parametrize(
-        "rule_runner", (OnlyBuiltinsRule,), indirect=["rule_runner"]
-    )
-    def test_only_builtin_fail(rule_runner: RunFromText) -> None:
+    def test_only_builtin_fail() -> None:
         """Test rule matches."""
-        results = rule_runner.run_playbook(FAIL_PLAY)
-        assert len(results) == 1
-        for result in results:
-            assert result.message == OnlyBuiltinsRule().shortdesc
+        result = run_ansible_lint(
+            "--config-file=/dev/null",
+            "--warn-list=",
+            "--enable-list",
+            "only-builtins",
+            "examples/playbooks/rule-only-builtins.yml",
+        )
+        assert result.returncode == VIOLATIONS_FOUND_RC
+        assert "Finished with 1 failure(s)" in result.stderr
+        assert "only-builtins" in result.stdout
 
     @pytest.mark.parametrize(
         "rule_runner", (OnlyBuiltinsRule,), indirect=["rule_runner"]

.pre-commit-config.yaml
@@ -124,7 +124,7 @@ repos:
         # empty args needed in order to match mypy cli behavior
         args: [--strict]
         additional_dependencies:
-          - ansible-compat>=2.0.2
+          - ansible-compat>=2.0.3
           - ansible-core
           - enrich
           - flaky
@@ -149,7 +149,7 @@ repos:
     hooks:
       - id: pylint
         additional_dependencies:
-          - ansible-compat>=2.0.2
+          - ansible-compat>=2.0.3
           - ansible-core
           - docutils
           - enrich

examples/playbooks/rule-only-builtins.yml
@@ -1,0 +1,9 @@
+---
+- hosts: localhost
+  tasks:
+    - name: sysctl
+      # while next module is mocked in our config, we still want to see that
+      # only-builtins rules gets matched
+      fake_namespace.fake_collection.fake_module:
+        name: vm.swappiness
+        value: "5"

setup.cfg
@@ -63,7 +63,7 @@ zip_safe = False
 
 # These are required in actual runtime:
 install_requires =
-  ansible-compat>=2.0.2  # GPLv3
+  ansible-compat>=2.0.3  # GPLv3
   ansible-core>=2.12.0  # GPLv3
   enrich>=1.2.6
   jsonschema>=4.5.1  # MIT, first version to have ordered keys in output

src/ansiblelint/__main__.py
@@ -162,7 +162,7 @@ def main(argv: Optional[List[str]] = None) -> int:
     _logger.debug("Options: %s", options)
     _logger.debug(os.getcwd())
 
-    app = get_app()
+    app = get_app(offline=options.offline)
     # pylint: disable=import-outside-toplevel
     from ansiblelint.rules import RulesCollection
     from ansiblelint.runner import _get_matches

src/ansiblelint/_mockings.py
@@ -53,7 +53,7 @@ def _write_module_stub(
         f.write(body)
 
 
-def _perform_mockings() -> None:
+def _perform_mockings() -> None:  # noqa: C901
     """Mock modules and roles."""
     for role_name in options.mock_roles:
         if re.match(r"\w+\.\w+\.\w+$", role_name):
@@ -84,7 +84,10 @@ def _perform_mockings() -> None:
     collections_path.mkdir(parents=True, exist_ok=True)
     link_path = collections_path / collection
     target = pathlib.Path(options.project_dir).absolute()
-    if not link_path.exists() or os.readlink(link_path) != target:
-        if link_path.exists():
+    try:
+        while os.readlink(link_path) != target:
             link_path.unlink()
+    except FileNotFoundError:
+        pass
+    if not link_path.exists():
         link_path.symlink_to(target, target_is_directory=True)

src/ansiblelint/app.py
@@ -242,12 +242,12 @@ def _sanitize_list_options(tag_list: List[str]) -> List[str]:
 
 
 @lru_cache(maxsize=1)
-def get_app() -> App:
+def get_app(offline: bool = False) -> App:
     """Return the application instance."""
     app = App(options=default_options)
     # Make linter use the cache dir from compat
     default_options.cache_dir = app.runtime.cache_dir
 
-    app.runtime.prepare_environment()
+    app.runtime.prepare_environment(offline=offline)
     _perform_mockings()
     return app

src/ansiblelint/rules/syntax_check.py
@@ -70,7 +70,7 @@ class AnsibleSyntaxCheckRule(AnsibleLintRule):
                 shell=False,  # needed when command is a list
                 universal_newlines=True,
                 check=False,
-                env=get_app().runtime.environ,
+                env=get_app(offline=True).runtime.environ,
             )
             result = []
         if run.returncode != 0:

commit 2a32d3e5e7c61d29862b68c3fad48e636b256b0f

Fix only builtins fail test
With `config-file=/dev/null` the linting fails with `syntax-check[specific]`
and the only-builtins part is never executed.
Use the global config which properly mocks the fake collection and
assert we raised a warning, not a failure.

Fixes: 6e31f19ca8c0322140222afdce66cdb57ad32699

src/ansiblelint/rules/only_builtins.py
@@ -54,10 +54,9 @@ if "pytest" in sys.modules:
       ansible.builtin.shell: echo This rule should not get matched by the only-builtins rule
     """
 
-    def test_only_builtin_fail() -> None:
+    def test_only_builtins_fail() -> None:
         """Test rule matches."""
         result = run_ansible_lint(
-            "--config-file=/dev/null",
             "--strict",
             "--warn-list=",
             "--enable-list",
@@ -66,8 +65,8 @@ if "pytest" in sys.modules:
         )
         assert result.returncode == VIOLATIONS_FOUND_RC
         assert "Failed" in result.stderr
-        assert "1 failure(s)" in result.stderr
-        assert "only-builtins" in result.stdout
+        assert "1 warning(s)" in result.stderr
+        assert "only-builtins: Use only builtin actions" in result.stdout
 
     def test_only_builtins_allow_collections() -> None:
         """Test rule doesn't match."""