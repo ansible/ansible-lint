ca261e21f12627f5499542220e79a93d8b51272c
Evgeni Golov <evgeni@golov.de>
2022-11-24 20:38:03 +0100

    Treat ansible.legacy the same way ansible.builtin (#2727)
    * Also accept ansible.legacy as a valid builtin alias
    
    * chore: auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Sorin Sbarnea <ssbarnea@redhat.com>

--- src/ansiblelint/rules/only_builtins.py
+++ src/ansiblelint/rules/only_builtins.py
@@ -26,7 +26,11 @@ class OnlyBuiltinsRule(AnsibleLintRule):
     ) -> bool | str:
         module = task["action"]["__ansible_module_original__"]
 
-        is_builtin = module.startswith("ansible.builtin.") or module in builtins
+        is_builtin = (
+            module.startswith("ansible.builtin.")
+            or module.startswith("ansible.legacy.")
+            or module in builtins
+        )
 
         is_manually_allowed = (
             any(
@@ -55,6 +59,8 @@ if "pytest" in sys.modules:
     block:
     - name: Shell (fqcn)
       ansible.builtin.shell: echo This rule should not get matched by the only-builtins rule
+    - name: Command with legacy FQCN
+      ansible.legacy.command: echo This rule should not get matched by the only-builtins rule
     """
 
     def test_only_builtins_fail() -> None:
