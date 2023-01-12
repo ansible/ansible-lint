67c274656cb84f11585710b1535770475824ddd8
Sorin Sbarnea <ssbarnea@redhat.com>
2023-01-12 11:17:00 +0000

    Improve test collection downloading

--- tools/install-reqs.sh
+++ tools/install-reqs.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 set -euo pipefail
-pushd examples/playbooks/collections
+pushd examples/playbooks/collections >/dev/null
 MISSING=()
+export ANSIBLE_COLLECTIONS_PATH=.
 for COLLECTION in ansible.posix community.general community.molecule;
 do
-    FILE=${COLLECTION//\./-}
-    if test -n "$(find . -maxdepth 1 -name '$FILE*' -print -quit)"
+    COL_NAME=${COLLECTION//\./-}
+    FILENAME=$(find . -maxdepth 1 -name "$COL_NAME*" -print -quit)
+    if test -n "$FILENAME"
     then
-        echo "Already cached $FILE"
+        echo "Already cached $COL_NAME as $FILENAME"
     else
         MISSING+=("${COLLECTION}")
     fi
     if [ ${#MISSING[@]} -ne 0 ]; then
         ansible-galaxy collection download -p . -v "${MISSING[@]}"
+        # Downloader will break our requirements.yml on download
+        git checkout -- requirements.yml
     fi
 done
 
 echo "Install requirements.yml ..."
 cat requirements.yml
 ansible-galaxy collection install -r requirements.yml -p .
-popd
+popd >/dev/null
