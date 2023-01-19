48629ad3a5bbdb0d1e244b9a77550f0407b28802
Sorin Sbarnea <ssbarnea@redhat.com>
2023-01-19 17:53:49 +0000

    Change treatment of files that fail to load structured data (#2919)

--- src/ansiblelint/constants.py
+++ src/ansiblelint/constants.py
@@ -1,5 +1,6 @@
 """Constants used by AnsibleLint."""
 import os.path
+from enum import Enum
 from typing import Literal
 
 DEFAULT_RULESDIR = os.path.join(os.path.dirname(__file__), "rules")
@@ -152,3 +153,15 @@ ROLE_IMPORT_ACTION_NAMES = {
 #
 # https://github.com/ansible/ansible-lint-action/issues/138
 GIT_CMD = ["git", "-c", f"safe.directory={os.getcwd()}"]
+
+
+class States(Enum):
+    """States used are used as sentinel values in various places."""
+
+    NOT_LOADED = "File not loaded"
+    LOAD_FAILED = "File failed to load"
+    UNKNOWN_DATA = "Unknown data"
+
+    def __bool__(self) -> bool:
+        """Ensure all states evaluate as False as booleans."""
+        return False
