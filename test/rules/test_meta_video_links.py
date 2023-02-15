8592a4f6a978f58f90306acda46d7d7f4fb7c266
Ajinkya Udgirkar <ajinkyaudgirkar@gmail.com>
2023-02-15 18:16:00 +0530

    Add full coverage to meta_video_links rule (#3036)
    Co-authored-by: Ajinkya Udgirkar <audgirka@redhat.com>

--- test/rules/test_meta_video_links.py
+++ test/rules/test_meta_video_links.py
@@ -8,11 +8,19 @@ galaxy_info:
   video_links:
   - url: https://www.youtube.com/watch?v=aWmRepTSFKs&feature=youtu.be
     title: Proper format
+  - url: https://drive.google.com/file/d/1spYR51l8SqQqvAhSdZE7/view
+    title: Check for VIDEO_REGEXP validity and break
   - https://www.youtube.com/watch?v=aWmRepTSFKs&feature=youtu.be
   - my_bad_key: https://www.youtube.com/watch?v=aWmRepTSFKs&feature=youtu.be
     title: This has a bad key
   - url: www.acme.com/vid
     title: Bad format of url
+"""
+
+META_NO_GALAXY_INFO = """
+galaxy_information:
+  video_links:
+  - url: https://www.youtube.com/watch?v=aWmRepTSFKs&feature=youtu.be
 """
 
 
@@ -26,3 +34,13 @@ def test_video_links() -> None:
     assert "Expected item in 'video_links' to be a dictionary" in str(results)
     assert "'video_links' to contain only keys 'url' and 'title'" in str(results)
     assert "URL format 'www.acme.com/vid' is not recognized" in str(results)
+
+
+def test_meta_video_links_no_galaxy_info() -> None:
+    """Test meta_video_links."""
+    collection = RulesCollection()
+    collection.register(MetaVideoLinksRule())
+    runner = RunFromText(collection)
+
+    results = runner.run_role_meta_main(META_NO_GALAXY_INFO)
+    assert len(results) == 0
