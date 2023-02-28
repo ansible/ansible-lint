03f6f5a6db6c9c14183a7f77147fc8756e7e2ac0
Ajinkya Udgirkar <audgirka@redhat.com>
2023-02-28 14:32:17 +0530

    Removed lines not required for code coverage (meta-no-info rule)

--- src/ansiblelint/rules/meta_no_info.py
+++ src/ansiblelint/rules/meta_no_info.py
@@ -70,11 +70,6 @@ class MetaMainHasInfoRule(AnsibleLintRule):
         if file.kind != "meta":
             return []
 
-        # since Ansible 2.10 we can add a meta/requirements.yml but
-        # we only want to match on meta/main.yml
-        if file.path.name != "main.yml":
-            return []
-
         galaxy_info = data.get("galaxy_info", False)
         if galaxy_info:
             return [
