96d1914c80d556647fc3ee887b60c0419af7901c
Sorin Sbarnea <ssbarnea@redhat.com>
2023-04-11 16:29:54 +0100

    Refactor meta-incorrect tests (#3266)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Ajinkya Udgirkar <ajinkyaudgirkar@gmail.com>

--- src/ansiblelint/rules/meta_incorrect.py
+++ src/ansiblelint/rules/meta_incorrect.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2018, Ansible Project
 from __future__ import annotations
 
+import sys
 from typing import TYPE_CHECKING
 
 from ansiblelint.constants import LINE_NUMBER_KEY
@@ -54,3 +55,24 @@ class MetaChangeFromDefaultRule(AnsibleLintRule):
                 )
 
         return results
+
+
+if "pytest" in sys.modules:
+    from ansiblelint.rules import RulesCollection  # pylint: disable=ungrouped-imports
+    from ansiblelint.runner import Runner  # pylint: disable=ungrouped-imports
+
+    def test_default_galaxy_info(
+        default_rules_collection: RulesCollection,
+    ) -> None:
+        """Test for meta-incorrect."""
+        results = Runner(
+            "examples/roles/meta_incorrect_fail", rules=default_rules_collection
+        ).run()
+        for result in results:
+            assert result.rule.id == "meta-incorrect"
+        assert len(results) == 4
+
+        assert "Should change default metadata: author" in str(results)
+        assert "Should change default metadata: description" in str(results)
+        assert "Should change default metadata: company" in str(results)
+        assert "Should change default metadata: license" in str(results)
