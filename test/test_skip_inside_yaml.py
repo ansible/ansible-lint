726cf25df9245f30fdee584e5732fda911f9e20e
Sorin Sbarnea <ssbarnea@redhat.com>
2023-04-11 11:35:51 +0100

    Refactor tests (#3256)
    Co-authored-by: Ajinkya Udgirkar <ajinkyaudgirkar@gmail.com>

--- test/test_skip_inside_yaml.py
+++ test/test_skip_inside_yaml.py
@@ -3,54 +3,18 @@ import pytest
 
 from ansiblelint.rules import RulesCollection
 from ansiblelint.runner import Runner
-from ansiblelint.testing import RunFromText, run_ansible_lint
-
-ROLE_TASKS = """\
----
-- ansible.builtin.debug:
-    msg: this should fail linting due lack of name
-- ansible.builtin.debug:  # noqa: unnamed-task
-    msg: this should pass due to noqa comment
-"""
-
-ROLE_TASKS_WITH_BLOCK = """\
----
-- name: Bad git 1  # noqa: latest[git]
-  action: ansible.builtin.git a=b c=d
-- name: Bad git 2
-  action: ansible.builtin.git a=b c=d
-- name: Block with rescue and always section
-  block:
-    - name: Bad git 3  # noqa: latest[git]
-      action: ansible.builtin.git a=b c=d
-    - name: Bad git 4
-      action: ansible.builtin.git a=b c=d
-  rescue:
-    - name: Bad git 5  # noqa: latest[git]
-      action: ansible.builtin.git a=b c=d
-    - name: Bad git 6
-      action: ansible.builtin.git a=b c=d
-  always:
-    - name: Bad git 7  # noqa: latest[git]
-      action: ansible.builtin.git a=b c=d
-    - name: Bad git 8
-      action: ansible.builtin.git a=b c=d
-"""
+from ansiblelint.testing import run_ansible_lint
 
 
-def test_role_tasks(default_text_runner: RunFromText) -> None:
-    """Check that role tasks can contain skips."""
-    results = default_text_runner.run_role_tasks_main(ROLE_TASKS)
-    assert len(results) == 1, results
-    assert results[0].linenumber == 2
-    assert results[0].tag == "name[missing]"
-    assert results[0].rule.id == "name"
-
-
-def test_role_tasks_with_block(default_text_runner: RunFromText) -> None:
+def test_role_tasks_with_block(default_rules_collection: RulesCollection) -> None:
     """Check that blocks in role tasks can contain skips."""
-    results = default_text_runner.run_role_tasks_main(ROLE_TASKS_WITH_BLOCK)
-    assert len(results) == 12
+    results = Runner(
+        "examples/playbooks/roles/fixture_1",
+        rules=default_rules_collection,
+    ).run()
+    assert len(results) == 4
+    for result in results:
+        assert result.tag == "latest[git]"
 
 
 @pytest.mark.parametrize(
