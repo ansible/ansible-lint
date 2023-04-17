552335014afcb3219a8f4eb131b0ec0559f04df4
Sorin Sbarnea <ssbarnea@redhat.com>
2023-04-17 15:46:31 +0100

    Ensure profile rules do not have experimental or opt-in tag
    Fixes: #3255

--- src/ansiblelint/rules/sanity.py
+++ src/ansiblelint/rules/sanity.py
@@ -22,7 +22,7 @@ class CheckSanityIgnoreFiles(AnsibleLintRule):
         "Identifies non-allowed entries in the `tests/sanity/ignore*.txt files."
     )
     severity = "MEDIUM"
-    tags = ["experimental"]
+    tags = []
     version_added = "v6.14.0"
 
     # Partner Engineering defines this list. Please contact PE for changes.
---
+++ test/test_config.py
@@ -0,0 +1,16 @@
+"""Tests for config module."""
+from ansiblelint.config import PROFILES
+from ansiblelint.rules import RulesCollection
+
+
+def test_profiles(default_rules_collection: RulesCollection) -> None:
+    """Test the rules included in profiles are valid."""
+    profile_banned_tags = set(["opt-in", "experimental"])
+    for name, data in PROFILES.items():
+        for profile_rule_id in data["rules"]:
+            for rule in default_rules_collection.rules:
+                if profile_rule_id == rule.id:
+                    forbidden_tags = profile_banned_tags & set(rule.tags)
+                    assert (
+                        not forbidden_tags
+                    ), f"Rule {profile_rule_id} from {name} profile cannot use {profile_banned_tags & set(rule.tags)} tag."
