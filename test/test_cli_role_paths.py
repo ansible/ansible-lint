ansible/ansible-lint

commit 8f026927b4020a9cb78d325331b68c4fff6fe3c8

Refactor project root directory detection code (#3295)

src/ansiblelint/cli.py
@@ -10,7 +10,7 @@ from collections.abc import Sequence
 from pathlib import Path
 from typing import Any, Callable
 
-from ansiblelint.config import DEFAULT_KINDS, DEFAULT_WARN_LIST, PROFILES
+from ansiblelint.config import DEFAULT_KINDS, DEFAULT_WARN_LIST, PROFILES, log_entries
 from ansiblelint.constants import (
     CUSTOM_RULESDIR_ENVVAR,
     DEFAULT_RULESDIR,
@@ -20,7 +20,7 @@ from ansiblelint.file_utils import (
     Lintable,
     abspath,
     expand_path_vars,
-    guess_project_dir,
+    find_project_root,
     normpath,
 )
 from ansiblelint.loaders import IGNORE_FILE
@@ -300,7 +300,7 @@ def get_cli_parser() -> argparse.ArgumentParser:
     parser.add_argument(
         "--project-dir",
         dest="project_dir",
-        default=".",
+        default=None,
         help="Location of project/repository, autodetected based on location "
         "of configuration file.",
     )
@@ -487,7 +487,7 @@ def merge_config(file_config: dict[Any, Any], cli_config: Namespace) -> Namespac
 
     scalar_map = {
         "loop_var_prefix": None,
-        "project_dir": ".",
+        "project_dir": None,
         "profile": None,
         "sarif_file": None,
     }
@@ -565,7 +565,6 @@ def get_config(arguments: list[str]) -> Namespace:
         )
 
     # save info about custom config file, as options.config_file may be modified by merge_config
-    has_custom_config = not options.config_file
 
     file_config = load_config(options.config_file)
 
@@ -573,9 +572,18 @@ def get_config(arguments: list[str]) -> Namespace:
 
     options.rulesdirs = get_rules_dirs(options.rulesdir, options.use_default_rules)
 
-    if has_custom_config and options.project_dir == ".":
-        project_dir = guess_project_dir(options.config_file)
+    if not options.project_dir:
+        project_dir, method = find_project_root(
+            srcs=options.lintables,
+            config_file=options.config_file,
+        )
         options.project_dir = os.path.expanduser(normpath(project_dir))
+        log_entries.append(
+            (
+                logging.INFO,
+                f"Identified `{project_dir}` as project root as project root containing {method}.",
+            ),
+        )
 
     if not options.project_dir or not os.path.exists(options.project_dir):
         raise RuntimeError(

src/ansiblelint/file_utils.py
@@ -4,12 +4,11 @@ from __future__ import annotations
 import copy
 import logging
 import os
-import pathlib
 import subprocess
 import sys
 from argparse import Namespace
 from collections import OrderedDict, defaultdict
-from collections.abc import Iterator
+from collections.abc import Iterator, Sequence
 from contextlib import contextmanager
 from pathlib import Path
 from tempfile import NamedTemporaryFile
@@ -20,7 +19,7 @@ from wcmatch.wcmatch import RECURSIVE, WcMatch
 from yaml.error import YAMLError
 
 from ansiblelint.config import BASE_KINDS, options
-from ansiblelint.constants import GIT_CMD, FileType, States
+from ansiblelint.constants import CONFIG_FILENAMES, GIT_CMD, FileType, States
 from ansiblelint.logger import warn_or_fail
 
 if TYPE_CHECKING:
@@ -467,49 +466,58 @@ def strip_dotslash_prefix(fname: str) -> str:
     return fname[2:] if fname.startswith("./") else fname
 
 
-def guess_project_dir(config_file: str | None) -> str:
-    """Return detected project dir or current working directory."""
-    path = None
-    if config_file is not None and config_file != "/dev/null":
-        target = pathlib.Path(config_file)
-        if target.exists():
-            # for config inside .config, we return the parent dir as project dir
-            cfg_path = target.parent
-            if cfg_path.parts[-1] == ".config":
-                path = str(cfg_path.parent.absolute())
-            else:
-                path = str(cfg_path.absolute())
+def find_project_root(
+    srcs: Sequence[str],
+    config_file: str | None = None,
+) -> tuple[Path, str]:
+    """Return a directory containing .git or ansible-lint config files.
 
-    if path is None:
-        try:
-            result = subprocess.run(
-                [*GIT_CMD, "rev-parse", "--show-toplevel"],
-                capture_output=True,
-                text=True,
-                check=True,
-            )
+    That directory will be a common parent of all files and directories
+    passed in `srcs`.
 
-            path = result.stdout.splitlines()[0]
-        except subprocess.CalledProcessError as exc:
-            if not (
-                exc.returncode == 128 and "fatal: not a git repository" in exc.stderr
-            ):
-                _logger.warning(
-                    "Failed to guess project directory using git: %s",
-                    exc.stderr.rstrip("\n"),
-                )
-        except FileNotFoundError as exc:
-            _logger.warning("Failed to locate command: %s", exc)
+    If no directory in the tree contains a marker that would specify it's the
+    project root, the root of the file system is returned.
 
-    if path is None:
-        path = os.getcwd()
+    Returns a two-tuple with the first element as the project root path and
+    the second element as a string describing the method by which the
+    project root was discovered.
+    """
+    directory = None
+    if not srcs:
+        srcs = [str(Path.cwd().resolve().absolute())]
+    path_srcs = [Path(Path.cwd(), src).resolve() for src in srcs]
 
-    _logger.info(
-        "Guessed %s as project root directory",
-        path,
+    if config_file:
+        cfg_files = [config_file]
+    else:
+        cfg_files = CONFIG_FILENAMES
+
+    # A list of lists of parents for each 'src'. 'src' is included as a
+    # "parent" of itself if it is a directory
+    src_parents = [
+        list(path.parents) + ([path] if path.is_dir() else []) for path in path_srcs
+    ]
+
+    common_base = max(
+        set.intersection(*(set(parents) for parents in src_parents)),
+        key=lambda path: path.parts,
     )
 
-    return path
+    for directory in (common_base, *common_base.parents):
+        if (directory / ".git").exists():
+            return directory, ".git directory"
+
+        if (directory / ".hg").is_dir():
+            return directory, ".hg directory"
+
+        for cfg_file in cfg_files:
+            if not os.path.isabs(cfg_file):
+                if (directory / cfg_file).is_file():
+                    return directory, str(cfg_file)
+
+    if not directory:
+        return Path.cwd(), "current working directory"
+    return directory, "file system root"
 
 
 def expand_dirs_in_lintables(lintables: set[Lintable]) -> None:

test/test_file_utils.py
@@ -19,7 +19,7 @@ from ansiblelint.file_utils import (
     Lintable,
     expand_path_vars,
     expand_paths_vars,
-    guess_project_dir,
+    find_project_root,
     normpath,
     normpath_path,
 )
@@ -299,21 +299,27 @@ def test_kinds(monkeypatch: MonkeyPatch, path: str, kind: FileType) -> None:
     assert lintable_detected.kind == result[lintable_expected.name]
 
 
-def test_guess_project_dir_tmp_path(tmp_path: Path) -> None:
-    """Verify guess_project_dir()."""
+def test_find_project_root_1(tmp_path: Path) -> None:
+    """Verify find_project_root()."""
+    # this matches black behavior in absence of any config files or .git/.hg  folders.
     with cwd(str(tmp_path)):
-        result = guess_project_dir(None)
-        assert result == str(tmp_path)
+        path, method = find_project_root([])
+        assert str(path) == "/"
+        assert method == "file system root"
 
 
-def test_guess_project_dir_dotconfig() -> None:
-    """Verify guess_project_dir()."""
+def test_find_project_root_dotconfig() -> None:
+    """Verify find_project_root()."""
+    # this expects to return examples folder as project root because this
+    # folder already has an .config/ansible-lint.yml file inside, which should
+    # be enough.
     with cwd("examples"):
         assert os.path.exists(
             ".config/ansible-lint.yml",
         ), "Test requires config file inside .config folder."
-        result = guess_project_dir(".config/ansible-lint.yml")
-        assert result == str(os.getcwd())
+        path, method = find_project_root([])
+        assert str(path) == str(os.getcwd())
+        assert method == ".config/ansible-lint.yml"
 
 
 BASIC_PLAYBOOK = """

src/ansiblelint/__main__.py
@@ -48,7 +48,7 @@ from ansiblelint.color import (
     reconfigure,
     render_yaml,
 )
-from ansiblelint.config import get_version_warning, options
+from ansiblelint.config import get_version_warning, log_entries, options
 from ansiblelint.constants import EXIT_CONTROL_C_RC, GIT_CMD, LOCK_TIMEOUT_RC
 from ansiblelint.file_utils import abspath, cwd, normpath
 from ansiblelint.loaders import load_ignore_txt
@@ -184,7 +184,7 @@ def support_banner() -> None:
         )
 
 
-# pylint: disable=too-many-branches,too-many-statements
+# pylint: disable=too-many-branches,too-many-statements,too-many-locals
 def main(argv: list[str] | None = None) -> int:  # noqa: C901
     """Linter CLI entry point."""
     # alter PATH if needed (venv support)
@@ -210,6 +210,8 @@ def main(argv: list[str] | None = None) -> int:  # noqa: C901
         support_banner()
 
     initialize_logger(options.verbosity)
+    for level, message in log_entries:
+        _logger.log(level, message)
     _logger.debug("Options: %s", options)
     _logger.debug(os.getcwd())
 

src/ansiblelint/config.py
@@ -139,6 +139,9 @@ used_old_tags: dict[str, str] = {}
 # Used to store collection list paths (with mock paths if needed)
 collection_list: list[str] = []
 
+# Used to store log messages before logging is initialized (level, message)
+log_entries: list[tuple[int, str]] = []
+
 
 def get_rule_config(rule_id: str) -> dict[str, Any]:
     """Get configurations for the rule ``rule_id``."""

src/ansiblelint/constants.py
@@ -156,6 +156,8 @@ ROLE_IMPORT_ACTION_NAMES = {
 # https://github.com/ansible/ansible-lint-action/issues/138
 GIT_CMD = ["git", "-c", f"safe.directory={os.getcwd()}"]
 
+CONFIG_FILENAMES = [".ansible-lint", ".config/ansible-lint.yml"]
+
 
 class States(Enum):
     """States used are used as sentinel values in various places."""

commit 74493b8539be32676c4e2fcc1fe55da359134254

Fix config path detection logic
Fixed bug introduced in #3295 which prevented the linter from
discovering the config file in a parent directory.

examples/roles/test-role/molecule/default/include-import-role.yml
@@ -1,5 +1,5 @@
 ---
-- name: test
+- name: Fixture for test_run_playbook test
   gather_facts: false
   hosts: all
   roles:

src/ansiblelint/cli.py
@@ -581,7 +581,7 @@ def get_config(arguments: list[str]) -> Namespace:
         log_entries.append(
             (
                 logging.INFO,
-                f"Identified `{project_dir}` as project root as project root containing {method}.",
+                f"Identified [filename]{project_dir}[/] as project root due [bold]{method}[/].",
             ),
         )
 

src/ansiblelint/file_utils.py
@@ -519,9 +519,14 @@ def find_project_root(
             return directory, ".hg directory"
 
         for cfg_file in cfg_files:
-            if not os.path.isabs(cfg_file):
-                if (directory / cfg_file).is_file():
-                    return directory, str(cfg_file)
+            # note that if cfg_file is already absolute, 'directory' is ignored
+            resolved_cfg_path = directory / cfg_file
+            if resolved_cfg_path.is_file():
+                if os.path.isabs(cfg_file):
+                    directory = Path(cfg_file).parent
+                    if directory.name == ".config":
+                        directory = directory.parent
+                return directory, f"config file {resolved_cfg_path}"
 
     if not directory:
         return Path.cwd(), "current working directory"

test/test_cli_role_paths.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import pytest
 
+from ansiblelint.constants import RC
 from ansiblelint.testing import run_ansible_lint
 from ansiblelint.text import strip_ansi_escape
 
@@ -90,8 +91,12 @@ def test_run_playbook(local_test_dir: str) -> None:
     env = os.environ.copy()
     env["ANSIBLE_ROLES_PATH"] = role_path
 
-    result = run_ansible_lint(lintable, cwd=cwd, env=env)
-    assert "Use shell only when shell functionality is required" in result.stdout
+    result = run_ansible_lint("-f", "pep8", lintable, cwd=cwd, env=env)
+    # All 4 failures are expected to be found inside the included role and not
+    # from the playbook given as argument.
+    assert result.returncode == RC.VIOLATIONS_FOUND
+    assert "tasks/main.yml:2: command-instead-of-shell" in result.stdout
+    assert "tasks/world.yml:2: name[missing]" in result.stdout
 
 
 @pytest.mark.parametrize(

test/test_file_utils.py
@@ -324,7 +324,7 @@ def test_find_project_root_dotconfig() -> None:
         ), "Test requires config file inside .config folder."
         path, method = find_project_root([])
         assert str(path) == str(os.getcwd())
-        assert method == ".config/ansible-lint.yml"
+        assert ".config/ansible-lint.yml" in method
 
 
 BASIC_PLAYBOOK = """