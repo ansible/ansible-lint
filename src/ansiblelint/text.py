2e14630ca0c16179fcd753e542eebbb593e7991a
Sorin Sbarnea <ssbarnea@redhat.com>
2023-08-30 16:53:29 +0100

    Avoid false positive with var-name rule on roles
    Fixes: #3683

--- src/ansiblelint/text.py
+++ src/ansiblelint/text.py
@@ -6,6 +6,7 @@ from functools import cache
 
 RE_HAS_JINJA = re.compile(r"{[{%#].*[%#}]}", re.DOTALL)
 RE_HAS_GLOB = re.compile("[][*?]")
+RE_IS_FQCN_OR_NAME = re.compile(r"^\w+(\.\w+\.\w+)?$")
 
 
 def strip_ansi_escape(data: str | bytes) -> str:
@@ -47,3 +48,9 @@ def has_jinja(value: str) -> bool:
 def has_glob(value: str) -> bool:
     """Return true if a string looks like having a glob pattern."""
     return bool(isinstance(value, str) and RE_HAS_GLOB.search(value))
+
+
+@cache
+def is_fqcn_or_name(value: str) -> bool:
+    """Return true if a string seems to be a module/filter old name or a fully qualified one."""
+    return bool(isinstance(value, str) and RE_IS_FQCN_OR_NAME.search(value))
