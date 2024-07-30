34e97126fadc0967c686645fb335cba48f933bb3
Dmitrij Zavražin <112724233+dmitrij-zavrazhin-1304@users.noreply.github.com>
2024-07-30 09:47:14 +0000

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -102,6 +102,7 @@ def test_get_version_warning_no_pip(mocker: MockerFixture) -> None:
 def test_nodeps(lintable: str) -> None:
     """Asserts ability to be called w/ or w/o venv activation."""
     env = os.environ.copy()
+    py_path = Path(sys.executable).parent
     proc = subprocess.run(
         [str(py_path / "ansible-lint"), '--nodeps=true', lintable],
         check=False,
@@ -111,7 +112,6 @@ def test_nodeps(lintable: str) -> None:
     )
     assert proc.returncode == 0, proc
     env["ANSIBLE_LINT_NODEPS"] = "1"
-    py_path = Path(sys.executable).parent
     proc = subprocess.run(
         [str(py_path / "ansible-lint"), lintable],
         check=False,
