c698292e1d3e58f691c150932d51a3d5daf76a0e
Tom Bosmans <tom.bosmans@be.ibm.com>
2025-04-02 15:09:53 +0200

    fix: support more than 1 level deep

--- src/ansiblelint/utils.py
+++ src/ansiblelint/utils.py
@@ -582,9 +582,9 @@ class HandleChildren:
         ]
         if len(role_name) > 1:
             # This ignores deeper structures than 1 level
-            possible_paths.append(path_dwim(basedir, os.path.join("roles", role_name[-2], role_name[-1])))
-            possible_paths.append(path_dwim(basedir, os.path.join(role_name[-2], role_name[-1])))
-            possible_paths.append(path_dwim(basedir, os.path.join("..", "..", role_name[-2], role_name[-1])))
+            possible_paths.append(path_dwim(basedir, os.path.join("roles", *role_name)))
+            possible_paths.append(path_dwim(basedir, os.path.join(*role_name)))
+            possible_paths.append(path_dwim(basedir, os.path.join("..", "..", *role_name)))
 
         for loc in self.app.runtime.config.default_roles_path:
             loc = os.path.expanduser(loc)
