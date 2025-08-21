e8d9d85e97faa1fd98a6a4b423c2d96cd228df29
simonLeary42 <71396965+simonLeary42@users.noreply.github.com>
2025-08-21 19:48:26 -0400

    fix rule list in error message (#4740)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Bradley A. Thornton <bthornto@redhat.com>

--- src/ansiblelint/rules/__init__.py
+++ src/ansiblelint/rules/__init__.py
@@ -531,12 +531,13 @@ class RulesCollection:
 
         return matches
 
-    def known_tags(self) -> list[str]:
-        """Return a list of known tags, without returning no sub-tags."""
+    def known_transform_tags(self) -> list[str]:
+        """Return a list of known tags of rules that implement transform()."""
         tags = set()
         for rule in self.rules:
-            tags.add(rule.id)
-            tags.update(rule.tags)
+            if issubclass(rule.__class__, TransformMixin):
+                tags.add(rule.id)
+                tags.update(rule.tags)
         return sorted(tags)
 
     def list_tags(self) -> str:
